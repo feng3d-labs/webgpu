d839611a045e543f87234d862616600d465794c0
feng <908087098@qq.com>
2024-07-27 00:24:31 +0800

    整理代码

--- src/caches/getIGPUSubmit.ts
+++ src/caches/getIGPUSubmit.ts
@@ -131,7 +131,6 @@ function getIComputePassEncoder(computePassEncoder: IGPUComputePassEncoder)
 
 import { IGPURenderBundleObject } from "../data/IGPURenderBundleObject";
 import { IGPURenderObject } from "../data/IGPURenderObject";
-import { getIGPURenderBundle } from "./getIGPURenderBundle";
 import { getIGPURenderObject } from "./getIGPURenderObject";
 import { getIGPURenderPass } from "./getIGPURenderPass";
 
@@ -269,3 +268,38 @@ export function getIGPUComputeStage(computeStage: IGPUProgrammableStage)
 }
 
 const computeStateMap = new Map<IGPUProgrammableStage, IGPUComputeStage>();
+
+import { IGPURenderBundleEncoderDescriptor } from "../data/IGPURenderBundleObject";
+import { IGPURenderPassDescriptor } from "../data/IGPURenderPassEncoder";
+import { getIRenderPassFormats } from "./getIGPURenderPass";
+
+export function getIGPURenderBundle(device: GPUDevice, renderBundleObject: IGPURenderBundleObject, renderPass: IGPURenderPassDescriptor)
+{
+    let gpuRenderBundleObject: IGPURenderBundleObject = gpuRenderBundleObjectMap.get(renderBundleObject);
+    if (gpuRenderBundleObject)
+    {
+        return gpuRenderBundleObject;
+    }
+
+    // 获取渲染通道附件纹理格式。
+    const { colorAttachmentTextureFormats, depthStencilAttachmentTextureFormat } = getIRenderPassFormats(device, renderPass);
+
+    const renderBundle: IGPURenderBundleEncoderDescriptor = {
+        ...renderBundleObject.renderBundle,
+        colorFormats: colorAttachmentTextureFormats,
+        depthStencilFormat: depthStencilAttachmentTextureFormat,
+        sampleCount: renderPass.multisample,
+    };
+
+    const renderObjects = renderBundleObject.renderObjects.map((v) => getIGPURenderObject(device, v, renderPass));
+
+    gpuRenderBundleObject = {
+        renderBundle,
+        renderObjects,
+    };
+
+    gpuRenderBundleObjectMap.set(renderBundleObject, gpuRenderBundleObject);
+
+    return gpuRenderBundleObject;
+}
+const gpuRenderBundleObjectMap = new WeakMap<IGPURenderBundleObject, IGPURenderBundleObject>();
--- src/internal.ts
+++ src/internal.ts
@@ -1,6 +1,5 @@
 export * from "./caches/getIGPURenderObject";
 export * from "./caches/getIGPURenderPass";
-export * from "./caches/getIGPURenderPipeline";
 export * from "./caches/getIGPUSubmit";
 export * from "./caches/getIGPUTexture";
 export * from "./caches/getWGSLReflectInfo";
