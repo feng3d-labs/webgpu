e4ea7634a9d9fd0722be118f1d4ae7908b8cb4b3
feng <908087098@qq.com>
2024-07-27 00:21:24 +0800

    整理代码

--- src/caches/getIGPURenderPipeline.ts
+++ src/caches/getIGPURenderPipeline.ts
@@ -1,13 +1,12 @@
 import { watcher } from "@feng3d/watcher";
-import { IGPUDepthStencilState, IGPURenderPipeline, IGPUVertexState } from "../data/IGPURenderObject";
+import { IGPUDepthStencilState, IGPUFragmentState, IGPURenderPipeline, IGPUVertexState } from "../data/IGPURenderObject";
 import { IGPURenderPassDescriptor } from "../data/IGPURenderPassEncoder";
 import { IGPUVertexAttributes } from "../data/IGPUVertexAttributes";
 import { IGPUVertexBuffer } from "../data/IGPUVertexBuffer";
 import { gpuVertexFormatMap } from "../types/VertexFormat";
 import { ChainMap } from "../utils/ChainMap";
-import { getIGPUFragmentState } from "./getIGPUFragmentState";
 import { getIGPUPipelineLayout } from "./getIGPUPipelineLayout";
-import { getIRenderPassDepthStencilAttachmentFormats } from "./getIGPURenderPass";
+import { getIRenderPassColorAttachmentFormats, getIRenderPassDepthStencilAttachmentFormats } from "./getIGPURenderPass";
 import { WGSLBindingResourceInfoMap, WGSLVertexAttributeInfo, getWGSLReflectInfo } from "./getWGSLReflectInfo";
 
 /**
@@ -275,3 +274,72 @@ function getVertexBuffers(attributeInfos: WGSLVertexAttributeInfo[], vertices: I
 
     return { vertexBufferLayouts, vertexBuffers };
 }
+
+/**
+ * 获取片段阶段完整描述。
+ *
+ * @param fragmentState 片段简单阶段。
+ * @param colorAttachmentTextureFormats 颜色附件格式。
+ * @returns 片段阶段完整描述。
+ */
+export function getIGPUFragmentState(device: GPUDevice, fragmentState: IGPUFragmentState, renderPass: IGPURenderPassDescriptor)
+{
+    if (!fragmentState)
+    {
+        return undefined;
+    }
+
+    let gpuFragmentState = fragmentStateMap.get([fragmentState, renderPass]);
+    if (!gpuFragmentState)
+    {
+        // 获取渲染通道附件纹理格式。
+        const colorAttachmentTextureFormats = getIRenderPassColorAttachmentFormats(device, renderPass);
+
+        const code = fragmentState.code;
+        let entryPoint = fragmentState.entryPoint;
+        if (!entryPoint)
+        {
+            const reflect = getWGSLReflectInfo(code);
+            console.assert(reflect.fragmentEntryList.length > 0, `WGSL着色器 ${code} 中不存在片元入口点。`);
+            entryPoint = reflect.fragmentEntryList[0].entryPoint;
+        }
+        else
+        {
+            // 验证着色器中包含指定片段入口函数。
+            const reflect = getWGSLReflectInfo(code);
+            console.assert(!!reflect.fragmentEntryMap[entryPoint], `WGSL着色器 ${code} 中不存在指定的片元入口点 ${entryPoint} 。`);
+        }
+
+        const targets = colorAttachmentTextureFormats.map((v, i) =>
+        {
+            if (!v) return undefined;
+
+            const gpuColorTargetState: GPUColorTargetState = { format: v };
+
+            const colorTargetState = fragmentState.targets?.[i];
+            if (colorTargetState)
+            {
+                Object.assign(gpuColorTargetState, colorTargetState);
+            }
+
+            return gpuColorTargetState;
+        });
+
+        gpuFragmentState = {
+            code,
+            entryPoint,
+            targets,
+        };
+
+        if (fragmentState.constants)
+        {
+            gpuFragmentState.constants = fragmentState.constants;
+        }
+
+        fragmentStateMap.set([fragmentState, renderPass], gpuFragmentState);
+    }
+
+    return gpuFragmentState;
+}
+
+const fragmentStateMap = new ChainMap<[IGPUFragmentState, IGPURenderPassDescriptor], IGPUFragmentState>();
--- src/caches/getIGPUSubmit.ts
+++ src/caches/getIGPUSubmit.ts
@@ -4,7 +4,6 @@ import { IGPUCopyBufferToBuffer } from "../data/IGPUCopyBufferToBuffer";
 import { IGPUCopyTextureToTexture } from "../data/IGPUCopyTextureToTexture";
 import { IGPURenderPassEncoder } from "../data/IGPURenderPassEncoder";
 import { IGPUSubmit } from "../data/IGPUSubmit";
-import { getIGPURenderPassEncoder } from "./getIGPURenderPassEncoder";
 
 export function getIGPUSubmit(device: GPUDevice, data: IGPUSubmit)
 {
@@ -116,8 +115,6 @@ function getIGPUCopyBufferToBuffer(v: IGPUCopyBufferToBuffer)
     return gpuCopyTextureToTexture;
 }
 
-import { getIGPUComputeObject } from "./getIGPUComputeObject";
-
 function getIComputePassEncoder(computePassEncoder: IGPUComputePassEncoder)
 {
     const computePass = computePassEncoder.computePass;
@@ -131,3 +128,144 @@ function getIComputePassEncoder(computePassEncoder: IGPUComputePassEncoder)
 
     return gpuComputePassEncoder;
 }
+
+import { IGPURenderBundleObject } from "../data/IGPURenderBundleObject";
+import { IGPURenderObject } from "../data/IGPURenderObject";
+import { getIGPURenderBundle } from "./getIGPURenderBundle";
+import { getIGPURenderObject } from "./getIGPURenderObject";
+import { getIGPURenderPass } from "./getIGPURenderPass";
+
+function getIGPURenderPassEncoder(device: GPUDevice, renderPassEncoder: IGPURenderPassEncoder)
+{
+    const renderPass = getIGPURenderPass(device, renderPassEncoder.renderPass);
+
+    const renderObjects = renderPassEncoder.renderObjects.map((v) =>
+    {
+        if (isRenderBundle(v))
+        {
+            const gpuRenderObject = getIGPURenderBundle(device, v, renderPassEncoder.renderPass);
+
+            return gpuRenderObject;
+        }
+
+        const gpuRenderObject = getIGPURenderObject(device, v, renderPassEncoder.renderPass);
+
+        return gpuRenderObject;
+    });
+
+    const gpuRenderPassEncoder: IGPURenderPassEncoder = {
+        renderPass,
+        renderObjects,
+    };
+
+    return gpuRenderPassEncoder;
+}
+
+function isRenderBundle(arg: IGPURenderObject | IGPURenderBundleObject): arg is IGPURenderBundleObject
+{
+    return !!(arg as IGPURenderBundleObject).renderObjects;
+}
+
+import { IGPUComputeObject, IGPUComputePipeline } from "../data/IGPUComputeObject";
+import { getIGPUPipelineLayout } from "./getIGPUPipelineLayout";
+import { getIGPUSetBindGroups } from "./getIGPUSetBindGroups";
+import { WGSLBindingResourceInfoMap } from "./getWGSLReflectInfo";
+
+function getIGPUComputeObject(renderObject: IGPUComputeObject)
+{
+    const { gpuComputePipeline, bindingResourceInfoMap } = getIGPUComputePipeline(renderObject.pipeline);
+
+    const gpuComputeObject: IGPUComputeObject = {
+        ...renderObject,
+        pipeline: gpuComputePipeline,
+    };
+
+    // 计算 bindGroups
+    const bindGroups = getIGPUSetBindGroups(gpuComputePipeline, renderObject.bindingResources, bindingResourceInfoMap);
+
+    gpuComputeObject.bindGroups = bindGroups;
+
+    return gpuComputeObject;
+}
+
+/**
+ * 从渲染管线描述、渲染通道描述以及完整的顶点属性数据映射获得完整的渲染管线描述以及顶点缓冲区数组。
+ *获取完整的计算管线描述。
+ *
+ * @param computePipeline 计算管线描述。
+ * @returns 完整的计算管线描述。
+ */
+export function getIGPUComputePipeline(computePipeline: IGPUComputePipeline)
+{
+    let result = computePipelineMap.get(computePipeline);
+    if (result)
+    {
+        return result;
+    }
+
+    const gpuComputeStage = getIGPUComputeStage(computePipeline.compute);
+
+    // 从GPU管线中获取管线布局。
+    const { gpuPipelineLayout, bindingResourceInfoMap } = getIGPUPipelineLayout(computePipeline);
+
+    const gpuComputePipeline: IGPUComputePipeline = {
+        ...computePipeline,
+        layout: gpuPipelineLayout,
+        compute: gpuComputeStage,
+    };
+
+    result = { gpuComputePipeline, bindingResourceInfoMap };
+
+    computePipelineMap.set(computePipeline, result);
+
+    return result;
+}
+
+const computePipelineMap = new Map<IGPUComputePipeline, { gpuComputePipeline: IGPUComputePipeline, bindingResourceInfoMap: WGSLBindingResourceInfoMap }>();
+
+import { IGPUComputeStage, IGPUProgrammableStage } from "../data/IGPUComputeObject";
+import { getWGSLReflectInfo } from "./getWGSLReflectInfo";
+
+/**
+ * 获取计算阶段完整描述。
+ *
+ * @param computeStage 计算阶段描述。
+ * @returns 计算阶段完整描述。
+ */
+export function getIGPUComputeStage(computeStage: IGPUProgrammableStage)
+{
+    let gpuComputeState = computeStateMap.get(computeStage);
+    if (!gpuComputeState)
+    {
+        const code = computeStage.code;
+        let entryPoint = computeStage.entryPoint;
+        if (!entryPoint)
+        {
+            const reflect = getWGSLReflectInfo(code);
+            console.assert(reflect.computeEntryList.length > 0, `WGSL着色器 ${code} 中不存在计算入口点。`);
+            entryPoint = reflect.computeEntryList[0].entryPoint;
+        }
+        else
+        {
+            // 验证着色器中包含指定片段入口函数。
+            const reflect = getWGSLReflectInfo(code);
+            console.assert(!!reflect.computeEntryMap[entryPoint], `WGSL着色器 ${code} 中不存在指定的计算入口点 ${entryPoint}`);
+        }
+
+        gpuComputeState = {
+            code,
+            entryPoint,
+        };
+
+        if (computeStage.constants)
+        {
+            gpuComputeState.constants = computeStage.constants;
+        }
+
+        computeStateMap.set(computeStage, gpuComputeState);
+    }
+
+    return gpuComputeState;
+}
+
+const computeStateMap = new Map<IGPUProgrammableStage, IGPUComputeStage>();
