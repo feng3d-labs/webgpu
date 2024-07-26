b454f0fc88c774e9de78ba27bf18446c0d0a1a1c
feng <908087098@qq.com>
2024-07-27 00:17:03 +0800

    整理代码

--- src/caches/getIGPUSubmit.ts
+++ src/caches/getIGPUSubmit.ts
@@ -1,5 +1,10 @@
+import { IGPUCommandEncoder, IGPUPassEncoder } from "../data/IGPUCommandEncoder";
+import { IGPUComputePassEncoder } from "../data/IGPUComputePassEncoder";
+import { IGPUCopyBufferToBuffer } from "../data/IGPUCopyBufferToBuffer";
+import { IGPUCopyTextureToTexture } from "../data/IGPUCopyTextureToTexture";
+import { IGPURenderPassEncoder } from "../data/IGPURenderPassEncoder";
 import { IGPUSubmit } from "../data/IGPUSubmit";
-import { getIGPUCommandEncoder } from "./getIGPUCommandEncoder";
+import { getIGPURenderPassEncoder } from "./getIGPURenderPassEncoder";
 
 export function getIGPUSubmit(device: GPUDevice, data: IGPUSubmit)
 {
@@ -10,4 +15,119 @@ export function getIGPUSubmit(device: GPUDevice, data: IGPUSubmit)
     };
 
     return gpuSubmit;
+}
+
+function getIGPUCommandEncoder(device: GPUDevice, v: IGPUCommandEncoder)
+{
+    const passEncoders = v.passEncoders.map((v) =>
+    {
+        let gpuPassEncoder: IGPUPassEncoder;
+        if (isIRenderPassEncoder(v))
+        {
+            gpuPassEncoder = getIGPURenderPassEncoder(device, v);
+        }
+        else if (isIComputePassEncoder(v))
+        {
+            gpuPassEncoder = getIComputePassEncoder(v);
+        }
+        else if (isICopyTextureToTexture(v))
+        {
+            gpuPassEncoder = getIGPUCopyTextureToTexture(v);
+        }
+        else if (isICopyBufferToBuffer(v))
+        {
+            gpuPassEncoder = getIGPUCopyBufferToBuffer(v);
+        }
+        else
+        {
+            throw `未处理通道编码器 ${v}`;
+        }
+
+        return gpuPassEncoder;
+    });
+
+    const gpuCommandEncoder: IGPUCommandEncoder = {
+        passEncoders,
+    };
+
+    return gpuCommandEncoder;
+}
+
+function isIComputePassEncoder(arg: any): arg is IGPUComputePassEncoder
+{
+    return !!(arg as IGPUComputePassEncoder).computeObjects;
+}
+
+function isIRenderPassEncoder(arg: any): arg is IGPURenderPassEncoder
+{
+    return !!(arg as IGPURenderPassEncoder).renderPass;
+}
+
+function isICopyTextureToTexture(arg: any): arg is IGPUCopyTextureToTexture
+{
+    return !!(arg as IGPUCopyTextureToTexture).source?.texture;
+}
+
+function isICopyBufferToBuffer(arg: any): arg is IGPUCopyBufferToBuffer
+{
+    const source = (arg as IGPUCopyBufferToBuffer).source;
+    const destination = (arg as IGPUCopyBufferToBuffer).destination;
+
+    // 缓冲区必定给出尺寸 或者 数据。
+    if (!(source.size || source.data)) return false;
+    if (!(destination.size || destination.data)) return false;
+
+    return true;
+}
+
+function getIGPUCopyTextureToTexture(v: IGPUCopyTextureToTexture)
+{
+    const sourceTexture = v.source.texture;
+    const destinationTexture = v.destination.texture;
+
+    const gpuCopyTextureToTexture: IGPUCopyTextureToTexture = {
+        ...v,
+        source: { texture: sourceTexture },
+        destination: { texture: destinationTexture },
+    };
+
+    return gpuCopyTextureToTexture;
+}
+
+function getIGPUCopyBufferToBuffer(v: IGPUCopyBufferToBuffer)
+{
+    const source = v.source;
+    const destination = v.destination;
+
+    let { sourceOffset, destinationOffset, size } = v;
+
+    if (sourceOffset === undefined) sourceOffset = 0;
+    if (destinationOffset === undefined) destinationOffset = 0;
+    if (size === undefined) size = source.size;
+
+    const gpuCopyTextureToTexture: IGPUCopyBufferToBuffer = {
+        source,
+        sourceOffset,
+        destination,
+        destinationOffset,
+        size,
+    };
+
+    return gpuCopyTextureToTexture;
+}
+
+import { getIGPUComputeObject } from "./getIGPUComputeObject";
+
+function getIComputePassEncoder(computePassEncoder: IGPUComputePassEncoder)
+{
+    const computePass = computePassEncoder.computePass;
+
+    const computeObjects = computePassEncoder.computeObjects.map((v) => getIGPUComputeObject(v));
+
+    const gpuComputePassEncoder: IGPUComputePassEncoder = {
+        computePass,
+        computeObjects,
+    };
+
+    return gpuComputePassEncoder;
 }
