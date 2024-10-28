566ce0d951225b879561c96e440498f3da5f5ff4
feng <908087098@qq.com>
2024-10-28 17:32:46 +0800

    1

--- src/data/IGPURenderPassEncoder.ts
+++ src/data/IGPURenderPassEncoder.ts
@@ -1,8 +1,7 @@
-import { IPassDescriptor, IRenderPass } from "@feng3d/renderer-common";
+import { IRenderPass } from "@feng3d/renderer-common";
 import { IGPURenderBundleObject } from "./IGPURenderBundleObject";
 import { IGPURenderObject } from "./IGPURenderObject";
 import { IGPUTextureView } from "./IGPUTextureView";
-import { IRenderPassColorAttachment } from "@feng3d/renderer-common/src/data/IRenderPassColorAttachment";
 
 /**
  * GPU渲染通道编码器。
@@ -29,7 +28,7 @@ export interface IGPURenderPass extends IRenderPass
  *
  * {@link GPURenderPassDescriptor}
  */
-export interface IGPURenderPassDescriptor extends IPassDescriptor, Omit<GPURenderPassDescriptor, "colorAttachments" | "depthStencilAttachment">
+export interface IGPURenderPassDescriptor extends Omit<GPURenderPassDescriptor, "colorAttachments" | "depthStencilAttachment">
 {
     /**
      * The set of {@link GPURenderPassColorAttachment} values in this sequence defines which
@@ -77,7 +76,7 @@ export interface IAttachmentSize
  *
  * {@link GPURenderPassColorAttachment}
  */
-export interface IGPURenderPassColorAttachment extends IRenderPassColorAttachment, Omit<GPURenderPassColorAttachment, "view" | "resolveTarget" | "loadOp" | "storeOp">
+export interface IGPURenderPassColorAttachment extends Omit<GPURenderPassColorAttachment, "view" | "resolveTarget" | "loadOp" | "storeOp">
 {
     /**
      * A {@link GPUTextureView} describing the texture subresource that will be output to for this
