79935d5c1bcdd2a5d53ded4b5a890e7f6461caf0
feng <908087098@qq.com>
2024-11-25 20:50:53 +0800

    getGPURenderBundle

--- src/runs/runRenderBundle.ts
+++ src/runs/runRenderBundle.ts
@@ -1,34 +1,42 @@
 import { IGPURenderBundleObject } from "../data/IGPURenderBundleObject";
 import { IGPURenderPassFormat } from "../internal/IGPURenderPassFormat";
+import { ChainMap } from "../utils/ChainMap";
 import { runRenderObject } from "./runRenderObject";
 
 export function runRenderBundle(device: GPUDevice, passEncoder: GPURenderPassEncoder, renderPassFormat: IGPURenderPassFormat, renderBundleObject: IGPURenderBundleObject)
 {
-    let gRenderBundle: GPURenderBundle = renderBundleObject[_GPURenderBundle];
-    if (!gRenderBundle)
-    {
-        //
-        const descriptor: GPURenderBundleEncoderDescriptor = {
-            ...renderBundleObject.descriptor,
-            colorFormats: renderPassFormat.colorFormats,
-            depthStencilFormat: renderPassFormat.depthStencilFormat,
-            sampleCount: renderPassFormat.multisample,
-        };
-
-        //
-        const renderBundleEncoder = device.createRenderBundleEncoder(descriptor);
-
-        renderBundleObject.renderObjects.forEach((renderObject) =>
-        {
-            runRenderObject(device, renderBundleEncoder, renderPassFormat, renderObject);
-        });
-
-        gRenderBundle = renderBundleEncoder.finish();
-
-        renderBundleObject[_GPURenderBundle] = gRenderBundle;
-    }
+    const gRenderBundle = getGPURenderBundle(device, renderBundleObject, renderPassFormat);
 
     passEncoder.executeBundles([gRenderBundle]);
 }
 
-const _GPURenderBundle = "_GPURenderBundle";
+export function getGPURenderBundle(device: GPUDevice, renderBundleObject: IGPURenderBundleObject, renderPassFormat: IGPURenderPassFormat)
+{
+    const map: ChainMap<[IGPURenderBundleObject, IGPURenderPassFormat], GPURenderBundle> = device[_RenderBundleMap] = device[_RenderBundleMap] || new ChainMap();
+    //
+    let gpuRenderBundle: GPURenderBundle = map.get([renderBundleObject, renderPassFormat]);
+    if (gpuRenderBundle) return gpuRenderBundle;
+
+    //
+    const descriptor: GPURenderBundleEncoderDescriptor = {
+        ...renderBundleObject.descriptor,
+        colorFormats: renderPassFormat.colorFormats,
+        depthStencilFormat: renderPassFormat.depthStencilFormat,
+        sampleCount: renderPassFormat.multisample,
+    };
+
+    //
+    const renderBundleEncoder = device.createRenderBundleEncoder(descriptor);
+
+    renderBundleObject.renderObjects.forEach((renderObject) =>
+    {
+        runRenderObject(device, renderBundleEncoder, renderPassFormat, renderObject);
+    });
+
+    gpuRenderBundle = renderBundleEncoder.finish();
+    map.set([renderBundleObject, renderPassFormat], gpuRenderBundle);
+
+    return gpuRenderBundle;
+}
+
+const _RenderBundleMap = "_RenderBundleMap";
