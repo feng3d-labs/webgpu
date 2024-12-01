363dd773cd9ef67e50e27403093f3a0707fb0c93
feng <908087098@qq.com>
2024-12-02 03:49:33 +0800

    还原 context.configuration.alphaMode 默认值

--- src/caches/getGPUCanvasContext.ts
+++ src/caches/getGPUCanvasContext.ts
@@ -17,7 +17,6 @@ export function getGPUCanvasContext(device: GPUDevice, context: IGPUCanvasContex
 
         //
         context.configuration = context.configuration || {};
-        context.configuration.alphaMode ||= "premultiplied";
         const format = (context.configuration as any).format = context.configuration.format || navigator.gpu.getPreferredCanvasFormat();
 
         let usage = 0;
