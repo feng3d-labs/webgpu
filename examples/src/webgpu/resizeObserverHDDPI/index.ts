462ab53504ae5601e1f8ef2b07eb085240c3a91d
feng <908087098@qq.com>
2024-12-02 23:56:31 +0800

    新增示例 resizeObserverHDDPI

--- examples/src/webgpu/resizeObserverHDDPI/index.ts
+++ examples/src/webgpu/resizeObserverHDDPI/index.ts
@@ -1,183 +1,149 @@
 import { GUI } from 'dat.gui';
 import checkerWGSL from './checker.wgsl';
-import { quitIfWebGPUNotAvailable } from '../util';
 
-import { IGPURenderObject, IGPURenderPassDescriptor, IGPUSubmit, WebGPU } from "@feng3d/webgpu-renderer";
+import { IGPUBindingResources, IGPURenderPassDescriptor, IGPURenderPipeline, IGPUSubmit, WebGPU } from "@feng3d/webgpu-renderer";
 
 const init = async (canvas: HTMLCanvasElement) =>
 {
-  const adapter = await navigator.gpu?.requestAdapter();
-  const device = await adapter?.requestDevice();
-  quitIfWebGPUNotAvailable(adapter, device);
+    const webgpu = await new WebGPU().init();
 
-  const context = canvas.getContext('webgpu') as GPUCanvasContext;
-
-  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-
-  context.configure({
-    device,
-    format: presentationFormat,
-  });
-
-  const module = device.createShaderModule({
-    code: checkerWGSL,
-  });
-  const pipeline = device.createRenderPipeline({
-    layout: 'auto',
-    vertex: { module },
-    fragment: {
-      module,
-      targets: [
-        {
-          format: presentationFormat,
+    const pipeline: IGPURenderPipeline = {
+        vertex: { code: checkerWGSL, },
+        fragment: {
+            code: checkerWGSL,
         },
-      ],
-    },
-  });
+    };
 
-  // These offsets are in f32/u32 offset.
-  enum UniformOffset
-  {
-    color0 = 0,
-    color1 = 4,
-    size = 8,
-  }
+    const uni = {
+        color0: undefined,
+        color1: undefined,
+        size: undefined,
+    };
 
-  const uniformValuesAsF32 = new Float32Array(12); // 2 vec4fs, 1 u32, 3 padding
-  const uniformValuesAsU32 = new Uint32Array(uniformValuesAsF32.buffer);
-  const uniformBuffer = device.createBuffer({
-    size: uniformValuesAsF32.byteLength,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-  });
+    const bindGroup: IGPUBindingResources = {
+        uni: uni,
+    };
 
-  const bindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [
-      {
-        binding: 0,
-        resource: { buffer: uniformBuffer },
-      },
-    ],
-  });
+    const settings = {
+        color0: '#FF0000',
+        color1: '#00FFFF',
+        size: 1,
+        resizable: false,
+        fullscreen()
+        {
+            if (document.fullscreenElement)
+            {
+                document.exitFullscreen();
+            } else
+            {
+                document.body.requestFullscreen();
+            }
+        },
+    };
 
-  const settings = {
-    color0: '#FF0000',
-    color1: '#00FFFF',
-    size: 1,
-    resizable: false,
-    fullscreen()
+    const containerElem = document.querySelector('#container') as HTMLElement;
+
+    const gui = new GUI();
+    gui.addColor(settings, 'color0').onChange(frame);
+    gui.addColor(settings, 'color1').onChange(frame);
+    gui.add(settings, 'size', 1, 32, 1).name('checker size').onChange(frame);
+    gui.add(settings, 'fullscreen');
+    gui.add(settings, 'resizable').onChange(() =>
     {
-      if (document.fullscreenElement)
-      {
-        document.exitFullscreen();
-      } else
-      {
-        document.body.requestFullscreen();
-      }
-    },
-  };
+        const { resizable } = settings;
+        // Get these before we adjust the CSS because our canvas is sized in device pixels
+        // and so will expand if we stop constraining it with CSS
+        const width = containerElem.clientWidth;
+        const height = containerElem.clientHeight;
 
-  const containerElem = document.querySelector('#container') as HTMLElement;
+        containerElem.classList.toggle('resizable', resizable);
+        containerElem.classList.toggle('fit-container', !resizable);
 
-  const gui = new GUI();
-  gui.addColor(settings, 'color0').onChange(frame);
-  gui.addColor(settings, 'color1').onChange(frame);
-  gui.add(settings, 'size', 1, 32, 1).name('checker size').onChange(frame);
-  gui.add(settings, 'fullscreen');
-  gui.add(settings, 'resizable').onChange(() =>
-  {
-    const { resizable } = settings;
-    // Get these before we adjust the CSS because our canvas is sized in device pixels
-    // and so will expand if we stop constraining it with CSS
-    const width = containerElem.clientWidth;
-    const height = containerElem.clientHeight;
-
-    containerElem.classList.toggle('resizable', resizable);
-    containerElem.classList.toggle('fit-container', !resizable);
-
-    containerElem.style.width = resizable ? `${width}px` : '';
-    containerElem.style.height = resizable ? `${height}px` : '';
-  });
-
-  // Given a CSS color, returns the color in 0 to 1 RGBA values.
-  const cssColorToRGBA = (function ()
-  {
-    const ctx = new OffscreenCanvas(1, 1).getContext('2d', {
-      willReadFrequently: true,
+        containerElem.style.width = resizable ? `${width}px` : '';
+        containerElem.style.height = resizable ? `${height}px` : '';
     });
-    return function (color: string)
+
+    // Given a CSS color, returns the color in 0 to 1 RGBA values.
+    const cssColorToRGBA = (function ()
     {
-      ctx.clearRect(0, 0, 1, 1);
-      ctx.fillStyle = color;
-      ctx.fillRect(0, 0, 1, 1);
-      return [...ctx.getImageData(0, 0, 1, 1).data].map((v) => v / 255);
-    };
-  })();
-
-  function frame()
-  {
-    uniformValuesAsF32.set(cssColorToRGBA(settings.color0), UniformOffset.color0);
-    uniformValuesAsF32.set(cssColorToRGBA(settings.color1), UniformOffset.color1);
-    uniformValuesAsU32[UniformOffset.size] = settings.size;
-
-    device.queue.writeBuffer(uniformBuffer, 0, uniformValuesAsF32);
-
-    const commandEncoder = device.createCommandEncoder();
-
-    const renderPassDescriptor: GPURenderPassDescriptor = {
-      colorAttachments: [
+        const ctx = new OffscreenCanvas(1, 1).getContext('2d', {
+            willReadFrequently: true,
+        });
+        return function (color: string)
         {
-          view: context.getCurrentTexture().createView(),
-          clearValue: [0.2, 0.2, 0.2, 1.0],
-          loadOp: 'clear',
-          storeOp: 'store',
-        },
-      ],
-    };
+            ctx.clearRect(0, 0, 1, 1);
+            ctx.fillStyle = color;
+            ctx.fillRect(0, 0, 1, 1);
+            return [...ctx.getImageData(0, 0, 1, 1).data].map((v) => v / 255);
+        };
+    })();
 
-    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    passEncoder.setPipeline(pipeline);
-    passEncoder.setBindGroup(0, bindGroup);
-    passEncoder.draw(3);
-    passEncoder.end();
-
-    device.queue.submit([commandEncoder.finish()]);
-  }
-
-  function getDevicePixelContentBoxSize(entry: ResizeObserverEntry)
-  {
-    // Safari does not support devicePixelContentBoxSize
-    if (entry.devicePixelContentBoxSize)
+    function frame()
     {
-      return {
-        width: entry.devicePixelContentBoxSize[0].inlineSize,
-        height: entry.devicePixelContentBoxSize[0].blockSize,
-      };
-    } else
-    {
-      // These values not correct but they're as close as you can get in Safari
-      return {
-        width: entry.contentBoxSize[0].inlineSize * devicePixelRatio,
-        height: entry.contentBoxSize[0].blockSize * devicePixelRatio,
-      };
+        uni.color0 = cssColorToRGBA(settings.color0);
+        uni.color1 = cssColorToRGBA(settings.color1);
+        uni.size = settings.size;
+
+        const renderPassDescriptor: IGPURenderPassDescriptor = {
+            colorAttachments: [
+                {
+                    view: { texture: { context: { canvasId: canvas.id } } },
+                    clearValue: [0.2, 0.2, 0.2, 1.0],
+                    loadOp: 'clear',
+                    storeOp: 'store',
+                },
+            ],
+        };
+
+        const submit: IGPUSubmit = {
+            commandEncoders: [{
+                passEncoders: [{
+                    descriptor: renderPassDescriptor,
+                    renderObjects: [{
+                        pipeline: pipeline,
+                        bindingResources: bindGroup,
+                        draw: { vertexCount: 3 },
+                    }]
+                }]
+            }]
+        };
+        webgpu.submit(submit);
     }
-  }
 
-  const { maxTextureDimension2D } = device.limits;
-  const observer = new ResizeObserver(([entry]) =>
-  {
-    // Note: If you are using requestAnimationFrame you should
-    // only record the size here but set it in the requestAnimationFrame callback
-    // otherwise you'll get flicker when resizing.
-    const { width, height } = getDevicePixelContentBoxSize(entry);
+    function getDevicePixelContentBoxSize(entry: ResizeObserverEntry)
+    {
+        // Safari does not support devicePixelContentBoxSize
+        if (entry.devicePixelContentBoxSize)
+        {
+            return {
+                width: entry.devicePixelContentBoxSize[0].inlineSize,
+                height: entry.devicePixelContentBoxSize[0].blockSize,
+            };
+        } else
+        {
+            // These values not correct but they're as close as you can get in Safari
+            return {
+                width: entry.contentBoxSize[0].inlineSize * devicePixelRatio,
+                height: entry.contentBoxSize[0].blockSize * devicePixelRatio,
+            };
+        }
+    }
 
-    // A size of 0 will cause an error when we call getCurrentTexture.
-    // A size > maxTextureDimension2D will also an error when we call getCurrentTexture.
-    canvas.width = Math.max(1, Math.min(width, maxTextureDimension2D));
-    canvas.height = Math.max(1, Math.min(height, maxTextureDimension2D));
-    frame();
-  });
-  observer.observe(canvas);
+    const { maxTextureDimension2D } = webgpu.device.limits;
+    const observer = new ResizeObserver(([entry]) =>
+    {
+        // Note: If you are using requestAnimationFrame you should
+        // only record the size here but set it in the requestAnimationFrame callback
+        // otherwise you'll get flicker when resizing.
+        const { width, height } = getDevicePixelContentBoxSize(entry);
+
+        // A size of 0 will cause an error when we call getCurrentTexture.
+        // A size > maxTextureDimension2D will also an error when we call getCurrentTexture.
+        canvas.width = Math.max(1, Math.min(width, maxTextureDimension2D));
+        canvas.height = Math.max(1, Math.min(height, maxTextureDimension2D));
+        frame();
+    });
+    observer.observe(canvas);
 };
 
 const webgpuCanvas = document.getElementById("webgpu") as HTMLCanvasElement;
