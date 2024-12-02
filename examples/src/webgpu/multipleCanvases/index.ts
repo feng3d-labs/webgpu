bf957c18596592beb45ad7ef2f4300c1a4e9df91
feng <908087098@qq.com>
2024-12-03 00:45:30 +0800

    新增示例 multipleCanvases

--- examples/src/webgpu/multipleCanvases/index.ts
+++ examples/src/webgpu/multipleCanvases/index.ts
@@ -1,83 +1,82 @@
-import { mat4, mat3 } from 'wgpu-matrix';
+import { getIGPUBuffer, IGPUBindingResources, IGPUCanvasContext, IGPUPassEncoder, IGPURenderPassDescriptor, IGPURenderPipeline, IGPUSubmit, IGPUTexture, IGPUVertexAttributes, WebGPU } from "@feng3d/webgpu-renderer";
+import { mat3, mat4 } from 'wgpu-matrix';
 import { modelData } from './models';
-import { quitIfWebGPUNotAvailable } from '../util';
 
 type TypedArrayView = Float32Array | Uint32Array;
 
 function createBufferWithData(
-  device: GPUDevice,
-  data: TypedArrayView,
-  usage: number
-) {
-  const buffer = device.createBuffer({
-    size: data.byteLength,
-    usage: usage,
-  });
-  device.queue.writeBuffer(buffer, 0, data);
-  return buffer;
+    device: GPUDevice,
+    data: TypedArrayView,
+    usage: number
+)
+{
+    const buffer = device.createBuffer({
+        size: data.byteLength,
+        usage: usage,
+    });
+    device.queue.writeBuffer(buffer, 0, data);
+    return buffer;
 }
 
 type Model = {
-  vertexBuffer: GPUBuffer;
-  indexBuffer: GPUBuffer;
-  indexFormat: GPUIndexFormat;
-  vertexCount: number;
+    vertices: Float32Array;
+    indices: Uint32Array;
+    vertexAttributes: IGPUVertexAttributes,
 };
 
 function createVertexAndIndexBuffer(
-  device: GPUDevice,
-  { vertices, indices }: { vertices: Float32Array; indices: Uint32Array }
-): Model {
-  const vertexBuffer = createBufferWithData(
-    device,
-    vertices,
-    GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
-  );
-  const indexBuffer = createBufferWithData(
-    device,
-    indices,
-    GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST
-  );
-  return {
-    vertexBuffer,
-    indexBuffer,
-    indexFormat: 'uint32',
-    vertexCount: indices.length,
-  };
+    { vertices, indices }: { vertices: Float32Array; indices: Uint32Array }
+): Model
+{
+    const vertexAttributes: IGPUVertexAttributes = {
+        position: { data: vertices, format: "float32x3", offset: 0, arrayStride: 6 * 4 },
+        normal: { data: vertices, format: "float32x3", offset: 3 * 4, arrayStride: 6 * 4 },
+    };
+
+    return {
+        vertices,
+        indices,
+        vertexAttributes,
+    };
 }
 
-const adapter = await navigator.gpu?.requestAdapter();
-const device = await adapter?.requestDevice();
-quitIfWebGPUNotAvailable(adapter, device);
+const init = async () =>
+{
+    const webgpu = await new WebGPU().init();
 
-const models = Object.values(modelData).map((data) =>
-  createVertexAndIndexBuffer(device, data)
-);
+    const models = Object.values(modelData).map((data) =>
+        createVertexAndIndexBuffer(data)
+    );
 
-function rand(min?: number, max?: number) {
-  if (min === undefined) {
-    max = 1;
-    min = 0;
-  } else if (max === undefined) {
-    max = min;
-    min = 0;
-  }
-  return Math.random() * (max - min) + min;
-}
+    function rand(min?: number, max?: number)
+    {
+        if (min === undefined)
+        {
+            max = 1;
+            min = 0;
+        } else if (max === undefined)
+        {
+            max = min;
+            min = 0;
+        }
+        return Math.random() * (max - min) + min;
+    }
 
-function randInt(min: number, max?: number) {
-  return Math.floor(rand(min, max));
-}
+    function randInt(min: number, max?: number)
+    {
+        return Math.floor(rand(min, max));
+    }
 
-function randColor() {
-  return [rand(), rand(), rand(), 1];
-}
+    function randColor()
+    {
+        return [rand(), rand(), rand(), 1];
+    }
 
-const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-const depthFormat = 'depth24plus';
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+    const depthFormat = 'depth24plus';
 
-const module = device.createShaderModule({
-  code: `
+    const module = {
+        code: `
     struct Uniforms {
       worldViewProjectionMatrix: mat4x4f,
       worldMatrix: mat4x4f,
@@ -109,255 +108,231 @@ const module = device.createShaderModule({
       return vec4f(uni.color.rgb * light, uni.color.a);
     }
   `,
-});
+    };
 
-const pipeline = device.createRenderPipeline({
-  label: 'our hardcoded red triangle pipeline',
-  layout: 'auto',
-  vertex: {
-    module,
-    buffers: [
-      {
-        arrayStride: 6 * 4, // position, normal
-        attributes: [
-          {
-            // position
-            shaderLocation: 0,
-            offset: 0,
-            format: 'float32x3',
-          },
-          {
-            // normal
-            shaderLocation: 1,
-            offset: 3 * 4,
-            format: 'float32x3',
-          },
-        ],
-      },
-    ],
-  },
-  fragment: {
-    module,
-    targets: [{ format: presentationFormat }],
-  },
-  primitive: {
-    cullMode: 'back',
-  },
-  depthStencil: {
-    depthWriteEnabled: true,
-    depthCompare: 'less',
-    format: depthFormat,
-  },
-});
+    const pipeline: IGPURenderPipeline = {
+        label: 'our hardcoded red triangle pipeline',
+        vertex: {
+            ...module,
+        },
+        fragment: {
+            ...module,
+        },
+        primitive: {
+            cullMode: 'back',
+        },
+        depthStencil: {
+            depthWriteEnabled: true,
+            depthCompare: 'less',
+        },
+    };
 
-const resizeObserver = new ResizeObserver((entries) => {
-  for (const entry of entries) {
-    const canvas = entry.target as HTMLCanvasElement;
-    const width = entry.contentBoxSize[0].inlineSize;
-    const height = entry.contentBoxSize[0].blockSize;
-    canvas.width = Math.max(
-      1,
-      Math.min(width, device.limits.maxTextureDimension2D)
-    );
-    canvas.height = Math.max(
-      1,
-      Math.min(height, device.limits.maxTextureDimension2D)
-    );
-  }
-});
-
-const visibleCanvasSet = new Set<HTMLCanvasElement>();
-const intersectionObserver = new IntersectionObserver((entries) => {
-  for (const { target, isIntersecting } of entries) {
-    const canvas = target as HTMLCanvasElement;
-    if (isIntersecting) {
-      visibleCanvasSet.add(canvas);
-    } else {
-      visibleCanvasSet.delete(canvas);
-    }
-  }
-});
-
-type CanvasInfo = {
-  context: GPUCanvasContext;
-  depthTexture?: GPUTexture;
-  clearValue: number[];
-  worldViewProjectionMatrixValue: Float32Array;
-  worldMatrixValue: Float32Array;
-  uniformValues: Float32Array;
-  uniformBuffer: GPUBuffer;
-  bindGroup: GPUBindGroup;
-  rotation: number;
-  model: Model;
-};
-
-const outerElem = document.querySelector('#outer');
-const canvasToInfoMap = new Map<HTMLCanvasElement, CanvasInfo>();
-const numProducts = 200;
-for (let i = 0; i < numProducts; ++i) {
-  // making this
-  // <div class="product size?">
-  //   <canvas></canvas>
-  //   <div>Product#: ?</div>
-  // </div>
-  const canvas = document.createElement('canvas');
-  resizeObserver.observe(canvas);
-  intersectionObserver.observe(canvas);
-
-  const container = document.createElement('div');
-  container.className = `product size${randInt(4)}`;
-
-  const description = document.createElement('div');
-  description.textContent = `product#: ${i + 1}`;
-
-  container.appendChild(canvas);
-  container.appendChild(description);
-  outerElem.appendChild(container);
-
-  // Get a WebGPU context and configure it.
-  const context = canvas.getContext('webgpu');
-  context.configure({
-    device,
-    format: presentationFormat,
-  });
-
-  // Make a uniform buffer and type array views
-  // for our uniforms.
-  const uniformValues = new Float32Array(16 + 16 + 4);
-  const uniformBuffer = device.createBuffer({
-    size: uniformValues.byteLength,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-  });
-  const kWorldViewProjectionMatrixOffset = 0;
-  const kWorldMatrixOffset = 16;
-  const kColorOffset = 32;
-  const worldViewProjectionMatrixValue = uniformValues.subarray(
-    kWorldViewProjectionMatrixOffset,
-    kWorldViewProjectionMatrixOffset + 16
-  );
-  const worldMatrixValue = uniformValues.subarray(
-    kWorldMatrixOffset,
-    kWorldMatrixOffset + 15
-  );
-  const colorValue = uniformValues.subarray(kColorOffset, kColorOffset + 4);
-  colorValue.set(randColor());
-
-  // Make a bind group for this uniform
-  const bindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [{ binding: 0, resource: { buffer: uniformBuffer } }],
-  });
-
-  canvasToInfoMap.set(canvas, {
-    context,
-    clearValue: randColor(),
-    worldViewProjectionMatrixValue,
-    worldMatrixValue,
-    uniformValues,
-    uniformBuffer,
-    bindGroup,
-    rotation: rand(Math.PI * 2),
-    model: models[randInt(models.length)],
-  });
-}
-
-const renderPassDescriptor: GPURenderPassDescriptor = {
-  label: 'our basic canvas renderPass',
-  colorAttachments: [
+    const resizeObserver = new ResizeObserver((entries) =>
     {
-      view: undefined, // <- to be filled out when we render
-      clearValue: [0.3, 0.3, 0.3, 1],
-      loadOp: 'clear',
-      storeOp: 'store',
-    },
-  ],
-  depthStencilAttachment: {
-    view: undefined, // <- to be filled out when we render
-    depthClearValue: 1.0,
-    depthLoadOp: 'clear',
-    depthStoreOp: 'store',
-  },
+        for (const entry of entries)
+        {
+            const canvas = entry.target as HTMLCanvasElement;
+            const width = entry.contentBoxSize[0].inlineSize;
+            const height = entry.contentBoxSize[0].blockSize;
+            canvas.width = Math.max(
+                1,
+                Math.min(width, webgpu.device.limits.maxTextureDimension2D)
+            );
+            canvas.height = Math.max(
+                1,
+                Math.min(height, webgpu.device.limits.maxTextureDimension2D)
+            );
+        }
+    });
+
+    const visibleCanvasSet = new Set<HTMLCanvasElement>();
+    const intersectionObserver = new IntersectionObserver((entries) =>
+    {
+        for (const { target, isIntersecting } of entries)
+        {
+            const canvas = target as HTMLCanvasElement;
+            if (isIntersecting)
+            {
+                visibleCanvasSet.add(canvas);
+            } else
+            {
+                visibleCanvasSet.delete(canvas);
+            }
+        }
+    });
+
+    type CanvasInfo = {
+        context: IGPUCanvasContext;
+        depthTexture?: IGPUTexture;
+        clearValue: number[];
+        worldViewProjectionMatrixValue: Float32Array;
+        worldMatrixValue: Float32Array;
+        uniformValues: Float32Array;
+        bindGroup: IGPUBindingResources;
+        rotation: number;
+        model: Model;
+    };
+
+    const outerElem = document.querySelector('#outer');
+    const canvasToInfoMap = new Map<HTMLCanvasElement, CanvasInfo>();
+    const numProducts = 200;
+    for (let i = 0; i < numProducts; ++i)
+    {
+        // making this
+        // <div class="product size?">
+        //   <canvas></canvas>
+        //   <div>Product#: ?</div>
+        // </div>
+        const canvas = document.createElement('canvas');
+        resizeObserver.observe(canvas);
+        intersectionObserver.observe(canvas);
+
+        const container = document.createElement('div');
+        container.className = `product size${randInt(4)}`;
+
+        const description = document.createElement('div');
+        description.textContent = `product#: ${i + 1}`;
+
+        container.appendChild(canvas);
+        container.appendChild(description);
+        outerElem.appendChild(container);
+
+        // Get a WebGPU context and configure it.
+        canvas.id = canvas.id || `gpuCanvas___` + (globalThis["gpuCanvasAutoID"] = ~~globalThis["gpuCanvasAutoID"] + 1)
+        const context: IGPUCanvasContext = { canvasId: canvas.id };
+
+        // Make a uniform buffer and type array views
+        // for our uniforms.
+        const uniformValues = new Float32Array(16 + 16 + 4);
+        const kWorldViewProjectionMatrixOffset = 0;
+        const kWorldMatrixOffset = 16;
+        const kColorOffset = 32;
+        const worldViewProjectionMatrixValue = uniformValues.subarray(
+            kWorldViewProjectionMatrixOffset,
+            kWorldViewProjectionMatrixOffset + 16
+        );
+        const worldMatrixValue = uniformValues.subarray(
+            kWorldMatrixOffset,
+            kWorldMatrixOffset + 15
+        );
+        const colorValue = uniformValues.subarray(kColorOffset, kColorOffset + 4);
+        colorValue.set(randColor());
+
+        // Make a bind group for this uniform
+        const bindGroup: IGPUBindingResources = {
+            uni: {
+                bufferView: uniformValues,
+                worldViewProjectionMatrix: undefined,
+                worldMatrix: undefined,
+                color: undefined,
+            }
+        };
+        // device.createBindGroup({
+        //     layout: pipeline.getBindGroupLayout(0),
+        //     entries: [{ binding: 0, resource: { buffer: uniformBuffer } }],
+        // });
+
+        canvasToInfoMap.set(canvas, {
+            context,
+            clearValue: randColor(),
+            worldViewProjectionMatrixValue,
+            worldMatrixValue,
+            uniformValues,
+            bindGroup,
+            rotation: rand(Math.PI * 2),
+            model: models[randInt(models.length)],
+        });
+    }
+
+    function render(time: number)
+    {
+        time *= 0.001; // convert to seconds;
+
+        // make a command encoder to start encoding commands
+        const passEncoders: IGPUPassEncoder[] = [];
+
+        visibleCanvasSet.forEach((canvas) =>
+        {
+            const canvasInfo = canvasToInfoMap.get(canvas);
+            const {
+                context,
+                uniformValues,
+                worldViewProjectionMatrixValue,
+                worldMatrixValue,
+                bindGroup,
+                clearValue,
+                rotation,
+                model: { vertexAttributes, indices },
+            } = canvasInfo;
+
+            // Get the current texture from the canvas context and
+            // set it as the texture to render to.
+            const renderPassDescriptor: IGPURenderPassDescriptor = {
+                label: 'our basic canvas renderPass',
+                colorAttachments: [
+                    {
+                        view: { texture: { context } }, // <- to be filled out when we render
+                        clearValue: clearValue,
+                        loadOp: 'clear',
+                        storeOp: 'store',
+                    },
+                ],
+                depthStencilAttachment: {
+                    view: undefined, // <- to be filled out when we render
+                    depthClearValue: 1.0,
+                    depthLoadOp: 'clear',
+                    depthStoreOp: 'store',
+                },
+            };
+
+            const fov = (60 * Math.PI) / 180;
+            const aspect = canvas.clientWidth / canvas.clientHeight;
+            const projection = mat4.perspective(fov, aspect, 0.1, 100);
+
+            const view = mat4.lookAt(
+                [0, 30, 50], // eye
+                [0, 0, 0], // target
+                [0, 1, 0] // up
+            );
+
+            const viewProjection = mat4.multiply(projection, view);
+
+            const world = mat4.rotationY(time * 0.1 + rotation);
+            mat4.multiply(viewProjection, world, worldViewProjectionMatrixValue);
+            mat3.fromMat4(world, worldMatrixValue);
+
+            // Upload our uniform values.
+            const buffer = getIGPUBuffer(uniformValues);
+            const writeBuffers = buffer.writeBuffers || [];
+            writeBuffers.push({
+                data: uniformValues,
+            })
+            buffer.writeBuffers = writeBuffers;
+
+            // make a render pass encoder to encode render specific commands
+            passEncoders.push({
+                descriptor: renderPassDescriptor,
+                renderObjects: [{
+                    pipeline,
+                    vertices: vertexAttributes,
+                    indices,
+                    bindingResources: bindGroup,
+                    drawIndexed: { indexCount: indices.length },
+                }],
+            });
+        });
+
+        const submit: IGPUSubmit = {
+            commandEncoders: [{
+                passEncoders,
+            }]
+        }
+        webgpu.submit(submit);
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+
 };
 
-function render(time: number) {
-  time *= 0.001; // convert to seconds;
-
-  // make a command encoder to start encoding commands
-  const encoder = device.createCommandEncoder();
-
-  visibleCanvasSet.forEach((canvas) => {
-    const canvasInfo = canvasToInfoMap.get(canvas);
-    const {
-      context,
-      uniformBuffer,
-      uniformValues,
-      worldViewProjectionMatrixValue,
-      worldMatrixValue,
-      bindGroup,
-      clearValue,
-      rotation,
-      model: { vertexBuffer, indexBuffer, indexFormat, vertexCount },
-    } = canvasInfo;
-    let { depthTexture } = canvasInfo;
-
-    // Get the current texture from the canvas context and
-    // set it as the texture to render to.
-    const canvasTexture = context.getCurrentTexture();
-    renderPassDescriptor.colorAttachments[0].view = canvasTexture.createView();
-    renderPassDescriptor.colorAttachments[0].clearValue = clearValue;
-
-    // If we don't have a depth texture OR if its size is different
-    // from the canvasTexture when make a new depth texture
-    if (
-      !depthTexture ||
-      depthTexture.width !== canvasTexture.width ||
-      depthTexture.height !== canvasTexture.height
-    ) {
-      if (depthTexture) {
-        depthTexture.destroy();
-      }
-      depthTexture = device.createTexture({
-        size: [canvasTexture.width, canvasTexture.height],
-        format: 'depth24plus',
-        usage: GPUTextureUsage.RENDER_ATTACHMENT,
-      });
-      canvasInfo.depthTexture = depthTexture;
-    }
-    renderPassDescriptor.depthStencilAttachment.view =
-      depthTexture.createView();
-
-    const fov = (60 * Math.PI) / 180;
-    const aspect = canvas.clientWidth / canvas.clientHeight;
-    const projection = mat4.perspective(fov, aspect, 0.1, 100);
-
-    const view = mat4.lookAt(
-      [0, 30, 50], // eye
-      [0, 0, 0], // target
-      [0, 1, 0] // up
-    );
-
-    const viewProjection = mat4.multiply(projection, view);
-
-    const world = mat4.rotationY(time * 0.1 + rotation);
-    mat4.multiply(viewProjection, world, worldViewProjectionMatrixValue);
-    mat3.fromMat4(world, worldMatrixValue);
-
-    // Upload our uniform values.
-    device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
-
-    // make a render pass encoder to encode render specific commands
-    const pass = encoder.beginRenderPass(renderPassDescriptor);
-    pass.setPipeline(pipeline);
-    pass.setVertexBuffer(0, vertexBuffer);
-    pass.setIndexBuffer(indexBuffer, indexFormat);
-    pass.setBindGroup(0, bindGroup);
-    pass.drawIndexed(vertexCount);
-    pass.end();
-  });
-
-  const commandBuffer = encoder.finish();
-  device.queue.submit([commandBuffer]);
-
-  requestAnimationFrame(render);
-}
-requestAnimationFrame(render);
+init();
