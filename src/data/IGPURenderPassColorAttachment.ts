e7e541009d33e5ff487b1aed459517e8b679bd2a
feng <908087098@qq.com>
2024-12-12 20:19:38 +0800

    IGPURenderPassColorAttachment -> IRenderPassColorAttachment

--- src/data/IGPURenderPassColorAttachment.ts
+++ src/data/IGPURenderPassColorAttachment.ts
@@ -3,31 +3,31 @@ import { ITextureView } from "@feng3d/render-api";
 declare module "@feng3d/render-api"
 {
 
-  /**
-   * GPU渲染通道颜色附件。
-   *
-   * {@link GPURenderPassColorAttachment}
-   */
-  export interface IRenderPassColorAttachment
-  {
     /**
-     * A {@link GPUTextureView} describing the texture subresource that will be output to for this
-     * color attachment.
-     */
-    readonly view?: ITextureView;
-
-    /**
-     * The store operation to perform on {@link GPURenderPassColorAttachment#view}
-     * after executing the render pass.
+     * GPU渲染通道颜色附件。
      *
-      * 默认 `"store"` 。
-      */
-    readonly storeOp?: GPUStoreOp;
-
-    /**
-     * Indicates the depth slice index of {@link GPUTextureViewDimension#"3d"} {@link GPURenderPassColorAttachment#view}
-     * that will be output to for this color attachment.
+     * {@link GPURenderPassColorAttachment}
      */
-    readonly depthSlice?: GPUIntegerCoordinate;
-  }
+    export interface IRenderPassColorAttachment
+    {
+        /**
+         * A {@link GPUTextureView} describing the texture subresource that will be output to for this
+         * color attachment.
+         */
+        readonly view?: ITextureView;
+
+        /**
+         * The store operation to perform on {@link GPURenderPassColorAttachment#view}
+         * after executing the render pass.
+         *
+          * 默认 `"store"` 。
+          */
+        readonly storeOp?: GPUStoreOp;
+
+        /**
+         * Indicates the depth slice index of {@link GPUTextureViewDimension#"3d"} {@link GPURenderPassColorAttachment#view}
+         * that will be output to for this color attachment.
+         */
+        readonly depthSlice?: GPUIntegerCoordinate;
+    }
 }
