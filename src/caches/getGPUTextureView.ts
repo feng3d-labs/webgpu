3654ddeb947a1f6581b167a3fc25f68b1c9ffc50
feng <908087098@qq.com>
2025-03-19 21:59:20 +0800

    refactor(caches): 重构 getGPUTextureView 函数
    - 使用 computed 和 reactive 优化缓存逻辑
    - 提高代码的可读性和性能
    - 改进纹理视图的创建和销毁处理

--- src/caches/getGPUTextureView.ts
+++ src/caches/getGPUTextureView.ts
@@ -1,43 +1,40 @@
 import { anyEmitter } from "@feng3d/event";
-import { CanvasTexture, ChainMap, Texture, TextureView } from "@feng3d/render-api";
+import { ChainMap, computed, ComputedRef, reactive, Texture, TextureView } from "@feng3d/render-api";
 import { GPUTexture_destroy, GPUTextureView_destroy } from "../eventnames";
 import { getGPUTexture } from "./getGPUTexture";
 
 export function getGPUTextureView(device: GPUDevice, view: TextureView)
 {
-    if ((view.texture as CanvasTexture).context)
-    {
-        const texture = getGPUTexture(device, view.texture);
+    const getGPUTextureViewKey: GetGPUTextureViewKey = [device, view];
+    let result = getGPUTextureViewMap.get(getGPUTextureViewKey);
+    if (result) return result.value;
 
-        const textureView = texture.createView(view);
+    result = computed(() =>
+    {
+        // 监听
+        const r_view = reactive(view);
+        r_view.texture;
+
+        // 执行
+        const { texture } = view;
+        const gpuTexture = getGPUTexture(device, texture);
+        const textureView = gpuTexture.createView({
+            ...r_view,
+            dimension: r_view.dimension ?? (texture as Texture).dimension,
+        });
+        // 销毁纹理时清除对应的纹理视图。
+        anyEmitter.once(gpuTexture, GPUTexture_destroy, () =>
+        {
+            getGPUTextureViewMap.delete(getGPUTextureViewKey);
+            anyEmitter.emit(textureView, GPUTextureView_destroy);
+        });
 
         return textureView;
-        // texture["view"] = texture["view"] || texture.createView();
-
-        // return texture["view"];
-    }
-
-    //
-    const getGPUTextureViewKey: GetGPUTextureViewKey = [device, view];
-    let textureView = getGPUTextureViewMap.get(getGPUTextureViewKey);
-    if (textureView) return textureView;
-
-    //
-    const texture = view.texture as Texture;
-    const gpuTexture = getGPUTexture(device, texture);
-    const dimension = view.dimension ?? texture.dimension;
-
-    textureView = gpuTexture.createView({ ...view, dimension });
-    getGPUTextureViewMap.set(getGPUTextureViewKey, textureView);
-    // 销毁纹理时清除对应的纹理视图。
-    anyEmitter.once(gpuTexture, GPUTexture_destroy, () =>
-    {
-        getGPUTextureViewMap.delete(getGPUTextureViewKey);
-        anyEmitter.emit(textureView, GPUTextureView_destroy);
     });
+    getGPUTextureViewMap.set(getGPUTextureViewKey, result);
 
-    return textureView;
+    return result.value;
 }
 
 type GetGPUTextureViewKey = [device: GPUDevice, view: TextureView];
-const getGPUTextureViewMap = new ChainMap<GetGPUTextureViewKey, GPUTextureView>;
+const getGPUTextureViewMap = new ChainMap<GetGPUTextureViewKey, ComputedRef<GPUTextureView>>;
