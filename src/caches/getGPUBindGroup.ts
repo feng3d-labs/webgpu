363119509972d3efd8a1b32f48758291314a910c
feng <908087098@qq.com>
2025-03-20 13:51:53 +0800

    refactor(caches): 将缓冲区绑定相关函数移至 getGPUBindGroup.ts
    - 将 getBufferBindingInfo 和 updateBufferBinding 函数从 utils 文件夹移至 getGPUBindGroup.ts
    - 优化了缓冲区绑定的处理逻辑，支持数字类型的资源
    - 调整了相关引用和类型定义

--- src/caches/getGPUBindGroup.ts
+++ src/caches/getGPUBindGroup.ts
@@ -1,10 +1,9 @@
-import { BindingResources, BufferBinding, ChainMap, computed, ComputedRef, reactive, Sampler, TextureView } from "@feng3d/render-api";
-import { ResourceType, TypeInfo } from "wgsl_reflect";
+import { BindingResources, BufferBinding, BufferBindingInfo, ChainMap, computed, ComputedRef, reactive, Sampler, TextureView, UnReadonly } from "@feng3d/render-api";
+import { watcher } from "@feng3d/watcher";
+import { ArrayInfo, ResourceType, StructInfo, TemplateInfo, TypeInfo, VariableInfo } from "wgsl_reflect";
 import { VideoTexture } from "../data/VideoTexture";
 import { webgpuEvents } from "../eventnames";
 import { ExternalSampledTextureType } from "../types/TextureType";
-import { getBufferBindingInfo } from "../utils/getBufferBindingInfo";
-import { updateBufferBinding } from "../utils/updateBufferBinding";
 import { getGPUBuffer } from "./getGPUBuffer";
 import { getGPUSampler } from "./getGPUSampler";
 import { getGPUTextureView } from "./getGPUTextureView";
@@ -63,6 +62,7 @@ function getGPUBindingResource(device: GPUDevice, bindingResources: BindingResou
     let result = getGPUBindingResourceMap.get(getGPUBindingResourceKey);
     if (result) return result.value;
 
+    let numberBufferBinding: number[];
     result = computed(() =>
     {
         // 监听
@@ -70,19 +70,25 @@ function getGPUBindingResource(device: GPUDevice, bindingResources: BindingResou
         (r_bindingResources[name] as BufferBinding)?.bufferView;
 
         // 执行
-        const resource = bindingResources[name] as BufferBinding;
-        const bufferBinding = ((typeof resource === "number") ? [resource] : resource) as BufferBinding; // 值为number且不断改变时将可能会产生无数细碎gpu缓冲区。
-        const bufferBindingInfo = getBufferBindingInfo(type);
+        let resource = bindingResources[name];
+        // 当值为number时，将其视为一个数组。
+        if (typeof resource === "number")
+        {
+            numberBufferBinding ??= [];
+            numberBufferBinding[0] = resource;
+            resource = numberBufferBinding;
+        }
+        const bufferBinding = resource as BufferBinding; // 值为number且不断改变时将可能会产生无数细碎gpu缓冲区。
         // 更新缓冲区绑定的数据。
-        updateBufferBinding(bufferBindingInfo, bufferBinding);
+        updateBufferBinding(type, bufferBinding);
         //
         const gbuffer = getGBuffer(bufferBinding.bufferView);
         (gbuffer as any).label = gbuffer.label || (`BufferBinding ${name}`);
         //
         const buffer = getGPUBuffer(device, gbuffer);
 
-        const offset = resource.bufferView.byteOffset;
-        const size = resource.bufferView.byteLength;
+        const offset = bufferBinding.bufferView.byteOffset;
+        const size = bufferBinding.bufferView.byteLength;
 
         const gpuBindingResource: GPUBindingResource = {
             buffer,
@@ -120,3 +126,217 @@ function getGPUExternalTexture(device: GPUDevice, videoTexture: VideoTexture)
     return result.value;
 }
 const getGPUExternalTextureMap = new WeakMap<VideoTexture, ComputedRef<GPUExternalTexture>>();
+
+
+/**
+ * 初始化缓冲区绑定。
+ *
+ * @param variableInfo
+ * @param uniformData
+ * @returns
+ */
+export function updateBufferBinding(type: TypeInfo, uniformData: BufferBinding)
+{
+    const bufferBindingInfo = getBufferBindingInfo(type);
+    if (uniformData["_variableInfo"] !== undefined)
+    {
+        const preVariableInfo = uniformData["_variableInfo"] as any as VariableInfo;
+        if (preVariableInfo.size !== bufferBindingInfo.size)
+        {
+            console.warn(`updateBufferBinding ${preVariableInfo.name} 出现一份数据对应多个 variableInfo`, { uniformData, bufferBindingInfo, preVariableInfo });
+        }
+
+        return;
+    }
+    uniformData["_variableInfo"] = bufferBindingInfo as any;
+
+    const size = bufferBindingInfo.size;
+    // 是否存在默认值。
+    const hasDefautValue = !!uniformData.bufferView;
+    if (!hasDefautValue)
+    {
+        (uniformData as UnReadonly<BufferBinding>).bufferView = new Uint8Array(size);
+    }
+
+    const buffer = getGBuffer(uniformData.bufferView);
+    const offset = uniformData.bufferView.byteOffset;
+
+    for (let i = 0; i < bufferBindingInfo.items.length; i++)
+    {
+        const { paths, offset: itemInfoOffset, size: itemInfoSize, Cls } = bufferBindingInfo.items[i];
+        const update = () =>
+        {
+            let value: any = uniformData;
+            for (let i = 0; i < paths.length; i++)
+            {
+                value = value[paths[i]];
+                if (value === undefined)
+                {
+                    if (!hasDefautValue)
+                    {
+                        console.warn(`没有找到 统一块变量属性 ${paths.join(".")} 的值！`);
+                    }
+
+                    return;
+                }
+            }
+
+            let data: Float32Array | Int32Array | Uint32Array | Int16Array;
+            if (typeof value === "number")
+            {
+                data = new Cls([value]);
+            }
+            else if (value.constructor.name !== Cls.name)
+            {
+                data = new Cls(value as ArrayLike<number>);
+            }
+            else
+            {
+                data = value as any;
+            }
+
+            const writeBuffers = buffer.writeBuffers ?? [];
+            writeBuffers.push({ bufferOffset: offset + itemInfoOffset, data: data.buffer, dataOffset: data.byteOffset, size: Math.min(itemInfoSize, data.byteLength) });
+            reactive(buffer).writeBuffers = writeBuffers;
+        };
+
+        update();
+        watcher.watchchain(uniformData, paths.join("."), update, undefined, false);
+    }
+}
+
+/**
+ * 获取缓冲区绑定信息。
+ *
+ * @param type 类型信息。
+ * @returns
+ */
+function getBufferBindingInfo(type: TypeInfo)
+{
+    let result = bufferBindingInfoMap.get(type);
+    if (result) return result;
+    result = _getBufferBindingInfo(type);
+
+    bufferBindingInfoMap.set(type, result);
+    return result;
+}
+const bufferBindingInfoMap = new Map<TypeInfo, BufferBindingInfo>();
+
+/**
+ * 获取缓冲区绑定信息。
+ *
+ * @param type 类型信息。
+ * @param paths 当前路径。
+ * @param offset 当前编译。
+ * @param bufferBindingInfo 缓冲区绑定信息。
+ * @returns
+ */
+function _getBufferBindingInfo(type: TypeInfo, paths: string[] = [], offset = 0, bufferBindingInfo: BufferBindingInfo = { size: type.size, items: [] })
+{
+    if (type.isStruct)
+    {
+        const structInfo = type as StructInfo;
+        for (let i = 0; i < structInfo.members.length; i++)
+        {
+            const memberInfo = structInfo.members[i];
+            _getBufferBindingInfo(memberInfo.type, paths.concat(memberInfo.name), offset + memberInfo.offset, bufferBindingInfo);
+        }
+    }
+    else if (type.isArray)
+    {
+        const arrayInfo = type as ArrayInfo;
+        for (let i = 0; i < arrayInfo.count; i++)
+        {
+            _getBufferBindingInfo(arrayInfo.format, paths.concat(`${i}`), offset + i * arrayInfo.format.size, bufferBindingInfo);
+        }
+    }
+    else if (type.isTemplate)
+    {
+        const templateInfo = type as TemplateInfo;
+        const templateFormatName = templateInfo.format?.name;
+        bufferBindingInfo.items.push({
+            paths: paths.concat(),
+            offset,
+            size: templateInfo.size,
+            Cls: getTemplateDataCls(templateFormatName as any),
+        });
+    }
+    else
+    {
+        bufferBindingInfo.items.push({
+            paths: paths.concat(),
+            offset,
+            size: type.size,
+            Cls: getBaseTypeDataCls(type.name),
+        });
+    }
+
+    return bufferBindingInfo;
+}
+
+function getBaseTypeDataCls(baseTypeName: string)
+{
+    const dataCls = baseTypeDataCls[baseTypeName];
+
+    console.assert(!!dataCls, `baseTypeName必须为以下值 ${Object.keys(baseTypeDataCls)}`);
+
+    return dataCls;
+}
+
+/**
+ * @see https://gpuweb.github.io/gpuweb/wgsl/#vec2i
+ */
+const baseTypeDataCls: { [key: string]: DataCls } = {
+    i32: Int32Array,
+    u32: Uint32Array,
+    f32: Float32Array,
+    f16: Int16Array,
+    vec2i: Int32Array,
+    vec3i: Int32Array,
+    vec4i: Int32Array,
+    vec2u: Uint32Array,
+    vec3u: Uint32Array,
+    vec4u: Uint32Array,
+    vec2f: Float32Array,
+    vec3f: Float32Array,
+    vec4f: Float32Array,
+    vec2h: Int16Array,
+    vec3h: Int16Array,
+    vec4h: Int16Array,
+    mat2x2f: Float32Array,
+    mat2x3f: Float32Array,
+    mat2x4f: Float32Array,
+    mat3x2f: Float32Array,
+    mat3x3f: Float32Array,
+    mat3x4f: Float32Array,
+    mat4x2f: Float32Array,
+    mat4x3f: Float32Array,
+    mat4x4f: Float32Array,
+    mat2x2h: Float32Array,
+    mat2x3h: Float32Array,
+    mat2x4h: Float32Array,
+    mat3x2h: Float32Array,
+    mat3x3h: Float32Array,
+    mat3x4h: Float32Array,
+    mat4x2h: Float32Array,
+    mat4x3h: Float32Array,
+    mat4x4h: Float32Array,
+};
+
+function getTemplateDataCls(templateFormatName: "i32" | "u32" | "f32" | "f16")
+{
+    const dataCls = templateFormatDataCls[templateFormatName];
+
+    console.assert(!!dataCls, `templateFormatName必须为以下值 ${Object.keys(templateFormatDataCls)}`);
+
+    return dataCls;
+}
+const templateFormatDataCls: { [key: string]: DataCls } = {
+    i32: Int32Array,
+    u32: Uint32Array,
+    f32: Float32Array,
+    f16: Int16Array,
+};
+
+type DataCls = Float32ArrayConstructor | Int32ArrayConstructor | Uint32ArrayConstructor | Int16ArrayConstructor;
+
