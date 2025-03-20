199446c9967027ced96a88a7a3fca75d4a5c8f90
feng <908087098@qq.com>
2025-03-20 15:16:46 +0800

    refactor(src): 重构 updateBufferBinding 函数
    - 调整函数参数顺序，提高可读性
    - 引入缓存机制，避免重复计算
    - 优化内部逻辑，提高执行效率
    - 修复了一些潜在的 bug 和警告

--- src/caches/getGPUBindGroup.ts
+++ src/caches/getGPUBindGroup.ts
@@ -80,7 +80,7 @@ function getGPUBindingResource(device: GPUDevice, bindingResources: BindingResou
         }
         const bufferBinding = resource as BufferBinding; // 值为number且不断改变时将可能会产生无数细碎gpu缓冲区。
         // 更新缓冲区绑定的数据。
-        updateBufferBinding(type, bufferBinding);
+        updateBufferBinding(bufferBinding, type);
         //
         const gbuffer = getGBuffer(bufferBinding.bufferView);
         (gbuffer as any).label = gbuffer.label || (`BufferBinding ${name}`);
@@ -135,76 +135,76 @@ const getGPUExternalTextureMap = new WeakMap<VideoTexture, ComputedRef<GPUExtern
  * @param uniformData
  * @returns
  */
-export function updateBufferBinding(type: TypeInfo, uniformData: BufferBinding)
+export function updateBufferBinding(uniformData: BufferBinding, type: TypeInfo)
 {
-    const bufferBindingInfo = getBufferBindingInfo(type);
-    if (uniformData["_variableInfo"] !== undefined)
+    const updateBufferBindingKey: UpdateBufferBindingKey = [uniformData, type];
+    let result = updateBufferBindingMap.get(updateBufferBindingKey);
+    if (result) return result.value;
+
+    result = computed(() =>
     {
-        const preVariableInfo = uniformData["_variableInfo"] as any as VariableInfo;
-        if (preVariableInfo.size !== bufferBindingInfo.size)
+        const bufferBindingInfo = getBufferBindingInfo(type);
+
+        const size = bufferBindingInfo.size;
+        // 是否存在默认值。
+        const hasDefautValue = !!uniformData.bufferView;
+        if (!hasDefautValue)
         {
-            console.warn(`updateBufferBinding ${preVariableInfo.name} 出现一份数据对应多个 variableInfo`, { uniformData, bufferBindingInfo, preVariableInfo });
+            (uniformData as UnReadonly<BufferBinding>).bufferView = new Uint8Array(size);
         }
-    }
-    else
-    {
-        uniformData["_variableInfo"] = bufferBindingInfo as any;
-    }
 
-    const size = bufferBindingInfo.size;
-    // 是否存在默认值。
-    const hasDefautValue = !!uniformData.bufferView;
-    if (!hasDefautValue)
-    {
-        (uniformData as UnReadonly<BufferBinding>).bufferView = new Uint8Array(size);
-    }
+        const buffer = getGBuffer(uniformData.bufferView);
+        const offset = uniformData.bufferView.byteOffset;
 
-    const buffer = getGBuffer(uniformData.bufferView);
-    const offset = uniformData.bufferView.byteOffset;
-
-    for (let i = 0; i < bufferBindingInfo.items.length; i++)
-    {
-        const { paths, offset: itemInfoOffset, size: itemInfoSize, Cls } = bufferBindingInfo.items[i];
-        // 更新数据
-        computed(() =>
+        for (let i = 0; i < bufferBindingInfo.items.length; i++)
         {
-            // 监听
-            let value: any = reactive(uniformData);
-            for (let i = 0; i < paths.length; i++)
-            {
-                value = value[paths[i]];
-                if (value === undefined)
-                {
-                    if (!hasDefautValue)
-                    {
-                        console.warn(`没有找到 统一块变量属性 ${paths.join(".")} 的值！`);
-                    }
-
-                    return;
-                }
-            }
-
+            const { paths, offset: itemInfoOffset, size: itemInfoSize, Cls } = bufferBindingInfo.items[i];
             // 更新数据
-            let data: Float32Array | Int32Array | Uint32Array | Int16Array;
-            if (typeof value === "number")
+            computed(() =>
             {
-                data = new Cls([value]);
-            }
-            else if (value.constructor.name !== Cls.name)
-            {
-                data = new Cls(value as ArrayLike<number>);
-            }
-            else
-            {
-                data = value as any;
-            }
+                // 监听
+                let value: any = reactive(uniformData);
+                for (let i = 0; i < paths.length; i++)
+                {
+                    value = value[paths[i]];
+                    if (value === undefined)
+                    {
+                        if (!hasDefautValue)
+                        {
+                            console.warn(`没有找到 统一块变量属性 ${paths.join(".")} 的值！`);
+                        }
 
-            const writeBuffers = buffer.writeBuffers ?? [];
-            writeBuffers.push({ bufferOffset: offset + itemInfoOffset, data: data.buffer, dataOffset: data.byteOffset, size: Math.min(itemInfoSize, data.byteLength) });
-            reactive(buffer).writeBuffers = writeBuffers;
-        }).value;
-    }
+                        return;
+                    }
+                }
+
+                // 更新数据
+                let data: Float32Array | Int32Array | Uint32Array | Int16Array;
+                if (typeof value === "number")
+                {
+                    data = new Cls([value]);
+                }
+                else if (value.constructor.name !== Cls.name)
+                {
+                    data = new Cls(value as ArrayLike<number>);
+                }
+                else
+                {
+                    data = value as any;
+                }
+
+                const writeBuffers = buffer.writeBuffers ?? [];
+                writeBuffers.push({ bufferOffset: offset + itemInfoOffset, data: data.buffer, dataOffset: data.byteOffset, size: Math.min(itemInfoSize, data.byteLength) });
+                reactive(buffer).writeBuffers = writeBuffers;
+            }).value;
+        }
+    });
+    updateBufferBindingMap.set(updateBufferBindingKey, result);
+
+    return result.value;
 }
+type UpdateBufferBindingKey = [uniformData: BufferBinding, type: TypeInfo];
+const updateBufferBindingMap = new ChainMap<UpdateBufferBindingKey, ComputedRef<void>>();
 
 /**
  * 获取缓冲区绑定信息。
