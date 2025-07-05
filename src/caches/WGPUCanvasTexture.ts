95f95745b8ee6c20491b35b3ce4d304d9cea5877
feng <908087098@qq.com>
2025-07-05 17:38:59 +0800

    重构 WGPUCanvasTexture 类，优化响应式逻辑和纹理管理
    - 继承 ReactiveClass，增强响应式处理能力
    - 更新构造函数和静态方法，简化纹理实例的获取和销毁逻辑
    - 改进对纹理变化的监听，提升代码可读性和维护性
    - 移除冗余属性，确保资源管理的有效性

--- src/caches/WGPUCanvasTexture.ts
+++ src/caches/WGPUCanvasTexture.ts
@@ -1,10 +1,105 @@
-import { effect, reactive } from '@feng3d/reactivity';
-import { CanvasTexture, ChainMap } from '@feng3d/render-api';
+import { reactive } from '@feng3d/reactivity';
+import { CanvasContext, CanvasTexture, ChainMap } from '@feng3d/render-api';
 import { webgpuEvents } from '../eventnames';
+import { ReactiveClass } from '../ReactiveClass';
 import { GPUCanvasContextManager } from './GPUCanvasContextManager';
 
-export class WGPUCanvasTexture
+export class WGPUCanvasTexture extends ReactiveClass
 {
+    readonly gpuTexture: GPUTexture;
+
+    /**
+     * 是否失效
+     */
+    readonly invalid: boolean = true;
+
+    private readonly _device: GPUDevice;
+    private readonly _canvasTexture: CanvasTexture;
+
+    constructor(device: GPUDevice, canvasTexture: CanvasTexture)
+    {
+        super();
+
+        this._device = device;
+        this._canvasTexture = canvasTexture;
+
+        WGPUCanvasTexture._textureMap.set([device, canvasTexture], this);
+
+        const r_this = reactive(this);
+
+        {
+            const r_canvasTexture = reactive(canvasTexture);
+            const r_webgpuEvents = reactive(webgpuEvents);
+
+            let preGPUTexture: GPUTexture;
+            this.effect(() =>
+            {
+                if (!r_this.gpuTexture) return;
+
+                r_webgpuEvents.preSubmit;
+                r_canvasTexture._canvasSizeVersion;
+
+                //
+                if (preGPUTexture === this.gpuTexture)
+                {
+                    r_this.gpuTexture = null;
+                }
+
+                preGPUTexture = this.gpuTexture;
+            });
+        }
+
+        // 监听纹理变化
+        {
+            let preGPUTexture: GPUTexture;
+            this.effect(() =>
+            {
+                r_this.gpuTexture;
+
+                preGPUTexture?.destroy();
+                preGPUTexture = this.gpuTexture;
+
+                if (!this.gpuTexture)
+                {
+                    r_this.invalid = true;
+                }
+            });
+        }
+    }
+
+    update()
+    {
+        if (this.invalid) return;
+
+        const r_this = reactive(this);
+
+        if (!this.gpuTexture)
+        {
+            r_this.gpuTexture = WGPUCanvasTexture._getCanvasGPUTexture(this._device, this._canvasTexture.context);
+        }
+
+        r_this.invalid = true;
+    }
+
+    static _getCanvasGPUTexture(device: GPUDevice, canvasContext: CanvasContext)
+    {
+        const context = GPUCanvasContextManager.getGPUCanvasContext(device, canvasContext);
+
+        const gpuTexture = context.getCurrentTexture();
+
+        gpuTexture.label = 'GPU画布纹理';
+
+        return gpuTexture;
+    }
+
+    destroy()
+    {
+        //
+        WGPUCanvasTexture._textureMap.delete([this._device, this._canvasTexture]);
+
+        super.destroy();
+    }
+
     static getInstance(device: GPUDevice, canvasTexture: CanvasTexture)
     {
         return this._textureMap.get([device, canvasTexture]) || new WGPUCanvasTexture(device, canvasTexture);
@@ -16,76 +111,4 @@ export class WGPUCanvasTexture
     }
 
     private static readonly _textureMap = new ChainMap<[GPUDevice, CanvasTexture], WGPUCanvasTexture>();
-
-    constructor(private device: GPUDevice, private texture: CanvasTexture)
-    {
-        WGPUCanvasTexture._textureMap.set([device, texture], this);
-
-        this.init();
-    }
-
-    get gpuTexture()
-    {
-        if (this.gpuTextureInvalid)
-        {
-            this._gpuTexture = this.getGPUTexture();
-            reactive(this).gpuTextureInvalid = false;
-        }
-
-        return this._gpuTexture;
-    }
-
-    private _gpuTexture: GPUTexture;
-
-    readonly gpuTextureInvalid: boolean = true;
-
-    init()
-    {
-        const r_this = reactive(this);
-        const canvasTexture = this.texture;
-
-        const r_webgpuEvents = reactive(webgpuEvents);
-        const r_canvasTexture = reactive(canvasTexture);
-
-        effect(() =>
-        {
-            if (r_this.gpuTextureInvalid) return;
-
-            r_webgpuEvents.preSubmit;
-            r_canvasTexture._canvasSizeVersion;
-
-            //
-            r_this.gpuTextureInvalid = true;
-        });
-
-        effect(() =>
-        {
-            r_this.gpuTextureInvalid;
-
-            if (!this.gpuTextureInvalid) return;
-            if (!this._gpuTexture) return;
-
-            this._gpuTexture.destroy();
-            this._gpuTexture = null;
-        });
-    }
-
-    getGPUTexture()
-    {
-        const { device, texture } = this;
-
-        const context = GPUCanvasContextManager.getGPUCanvasContext(device, texture.context);
-
-        const gpuTexture = context.getCurrentTexture();
-
-        gpuTexture.label = 'GPU画布纹理';
-
-        return gpuTexture;
-    }
-
-    destroy()
-    {
-        this._gpuTexture.destroy();
-        this._gpuTexture = null;
-    }
 }
