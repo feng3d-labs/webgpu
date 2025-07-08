d378e22b8af3e17faefb9f20a04821f27b652724
feng <908087098@qq.com>
2025-07-08 09:19:01 +0800

    重构 WGPUTextureView 类，优化纹理视图管理逻辑
    - 新增 update 方法，确保纹理视图的更新和管理更加高效
    - 引入响应式处理，提升对纹理状态变化的响应能力
    - 简化 GPU 纹理视图的创建和描述符管理，增强代码可读性
    - 移除冗余的静态方法，提升代码整洁性

--- src/caches/WGPUTextureView.ts
+++ src/caches/WGPUTextureView.ts
@@ -1,5 +1,6 @@
 import { effect, reactive } from '@feng3d/reactivity';
 import { ChainMap, Texture, TextureView } from '@feng3d/render-api';
+import { WGPUCanvasTexture } from './WGPUCanvasTexture';
 import { WGPUTexture } from './WGPUTexture';
 
 export class WGPUTextureView
@@ -15,29 +16,43 @@ export class WGPUTextureView
     {
         if (!view) return undefined;
 
-        let result = this.getGPUTextureViewMap.get([device, view]);
-
-        if (result) return result;
-
-        result = new WGPUTextureView(device, view);
-
-        this.getGPUTextureViewMap.set([device, view], result);
-
-        return result;
+        return this._gpuTextureViewMap.get([device, view]) || new WGPUTextureView(device, view);
     }
 
-    private static readonly getGPUTextureViewMap = new ChainMap<[device: GPUDevice, view: TextureView], WGPUTextureView>();
+    private static readonly _gpuTextureViewMap = new ChainMap<[device: GPUDevice, view: TextureView], WGPUTextureView>();
 
-    textureView: GPUTextureView
+    readonly textureView: GPUTextureView;
+
+    readonly descriptor: GPUTextureViewDescriptor;
+
+    readonly wgpuTexture: WGPUTexture | WGPUCanvasTexture;
+
+    readonly invalid: boolean = true;
+
+    private readonly _device: GPUDevice;
+    private readonly _view: TextureView;
 
     constructor(device: GPUDevice, view: TextureView)
     {
-        // 监听
+        WGPUTextureView._gpuTextureViewMap.set([device, view], this);
+
+        this._device = device;
+        this._view = view;
+
+        const r_this = reactive(this);
         const r_view = reactive(view);
 
         effect(() =>
         {
             r_view.texture;
+
+            r_this.wgpuTexture = null;
+            r_this.textureView = null;
+            r_this.invalid = true;
+        });
+
+        effect(() =>
+        {
             r_view.label;
             r_view.format;
             r_view.dimension;
@@ -48,22 +63,40 @@ export class WGPUTextureView
             r_view.mipLevelCount;
             r_view.arrayLayerCount;
 
-            // 执行
-            const { texture, label, format, dimension, usage, aspect, baseMipLevel, baseArrayLayer, mipLevelCount, arrayLayerCount } = view;
-            const gpuTexture = WGPUTexture.getInstance(device, texture);
-            const textureView = gpuTexture.gpuTexture.createView({
-                label: label ?? `${gpuTexture.gpuTexture.label}视图`,
-                format,
-                dimension: dimension ?? (texture as Texture).dimension,
-                usage,
-                aspect,
-                baseMipLevel,
-                mipLevelCount,
-                baseArrayLayer,
-                arrayLayerCount,
-            });
-
-            this.textureView = textureView;
+            r_this.descriptor = null;
+            r_this.textureView = null;
+            r_this.invalid = true;
         });
+    }
+
+    update()
+    {
+        if (!this.invalid) return;
+
+        const r_this = reactive(this);
+
+        if (!this.wgpuTexture)
+        {
+            r_this.wgpuTexture = WGPUTexture.getInstance(this._device, this._view.texture);
+        }
+        this.wgpuTexture.update();
+
+        if (!this.descriptor)
+        {
+            r_this.descriptor = {
+                ...this._view,
+                label: this._view.label ?? `${this.wgpuTexture.gpuTexture.label}视图`,
+                dimension: this._view.dimension ?? (this._view.texture as Texture).dimension,
+            }
+        }
+
+        if (!this.textureView)
+        {
+            r_this.textureView = this.wgpuTexture.gpuTexture.createView(this.descriptor);
+        }
+
+        r_this.invalid = false;
+
+        return this;
     }
 }
