ddd40053f42e3e76547d4545ef94d11b71456609
feng <908087098@qq.com>
2025-09-28 14:33:22 +0800

    优化 WGPUTimestampQuery 类，增加资源销毁逻辑，改进时间戳查询结果的处理流程，提升代码可读性和维护性。同时，确保在 GPU 提交事件中正确管理查询结果的获取与释放。

--- src/caches/WGPUTimestampQuery.ts
+++ src/caches/WGPUTimestampQuery.ts
@@ -41,6 +41,15 @@ export class WGPUTimestampQuery extends ReactiveObject
         // Create a buffer to map the result back to the CPU
         let resultBuf: GPUBuffer;
 
+        const destroy = () =>
+        {
+            resolveBuf?.destroy();
+            resultBuf?.destroy();
+
+            reactive(this).gpuPassTimestampWrites = null;
+        };
+
+        let needResolve = false;
         const resolve = (commandEncoder: GPUCommandEncoder) =>
         {
             resolveBuf = resolveBuf || device.createBuffer({
@@ -74,48 +83,66 @@ export class WGPUTimestampQuery extends ReactiveObject
                     0,
                     resultBuf.size,
                 );
+                needResolve = true;
             }
+        };
 
-            const getQueryResult = () =>
-            {
-                if (resultBuf.mapState === 'unmapped')
-                {
-                    resultBuf.mapAsync(GPUMapMode.READ).then(() =>
-                    {
-                        const timestamps = new BigUint64Array(resultBuf.getMappedRange());
-
-                        // Subtract the begin time from the end time.
-                        // Cast into number. Number can be 9007199254740991 as max integer
-                        // which is 109 days of nano seconds.
-                        const elapsedNs = Number(timestamps[1] - timestamps[0]);
-
-                        // It's possible elapsedNs is negative which means it's invalid
-                        // (see spec https://gpuweb.github.io/gpuweb/#timestamp)
-                        if (elapsedNs >= 0)
-                        {
-                            reactive(timestampQuery).result = { elapsedNs };
-                        }
-
-                        resultBuf.unmap();
-
-                        //
-                        anyEmitter.off(device.queue, GPUQueue_submit, getQueryResult);
-                    });
-                }
+        this.effect(() =>
+        {
+            const timestampWrites: GPURenderPassTimestampWrites | GPUComputePassTimestampWrites = {
+                querySet,
+                beginningOfPassWriteIndex: 0,
+                endOfPassWriteIndex: 1,
+                resolve,
             };
 
-            // 监听提交WebGPU事件
-            anyEmitter.on(device.queue, GPUQueue_submit, getQueryResult);
+            reactive(this).gpuPassTimestampWrites = timestampWrites;
+        });
+
+        const getQueryResult = () =>
+        {
+            if (!needResolve) return;
+
+            needResolve = false;
+
+            if (resultBuf.mapState === 'unmapped')
+            {
+                resultBuf.mapAsync(GPUMapMode.READ).then(() =>
+                {
+                    const timestamps = new BigUint64Array(resultBuf.getMappedRange());
+
+                    // Subtract the begin time from the end time.
+                    // Cast into number. Number can be 9007199254740991 as max integer
+                    // which is 109 days of nano seconds.
+                    const elapsedNs = Number(timestamps[1] - timestamps[0]);
+
+                    // It's possible elapsedNs is negative which means it's invalid
+                    // (see spec https://gpuweb.github.io/gpuweb/#timestamp)
+                    if (elapsedNs >= 0)
+                    {
+                        reactive(timestampQuery).result = { elapsedNs };
+                    }
+
+                    resultBuf.unmap();
+
+                    //
+                    anyEmitter.off(device.queue, GPUQueue_submit, getQueryResult);
+                });
+            }
         };
 
-        const timestampWrites: GPURenderPassTimestampWrites | GPUComputePassTimestampWrites = {
-            querySet,
-            beginningOfPassWriteIndex: 0,
-            endOfPassWriteIndex: 1,
-            resolve,
-        };
+        // 监听提交WebGPU事件
+        anyEmitter.on(device.queue, GPUQueue_submit, getQueryResult);
 
-        reactive(this).gpuPassTimestampWrites = timestampWrites;
+        this.effect(() =>
+        {
+
+
+
+        });
+
+        //
+        this.destroyCall(destroy);
     }
 
     private _onMap(device: GPUDevice, timestampQuery?: TimestampQuery)
