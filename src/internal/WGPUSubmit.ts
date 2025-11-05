fb1a5838957b553977f9772baa6208fada6a06a5
feng <908087098@qq.com>
2025-11-05 20:18:13 +0800

    refactor: 重构 WGPUSubmit 类以支持计算属性
    - 将 commandBuffers 属性改为计算属性，提升响应式更新能力
    - 移除冗余的 reactive 包装，简化代码逻辑
    - 更新实例管理逻辑，使用新的 ChainMap 管理方式

--- src/internal/WGPUSubmit.ts
+++ src/internal/WGPUSubmit.ts
@@ -1,73 +1,66 @@
 import { anyEmitter } from '@feng3d/event';
-import { reactive } from '@feng3d/reactivity';
-import { Submit } from '@feng3d/render-api';
+import { computed, reactive } from '@feng3d/reactivity';
+import { ChainMap, Submit } from '@feng3d/render-api';
 import { GPUQueue_submit } from '../eventnames';
 import { ReactiveObject } from '../ReactiveObject';
 import { WGPUCommandEncoder } from './WGPUCommandEncoder';
 
 export class WGPUSubmit extends ReactiveObject
 {
-    commandBuffers: WGPUCommandEncoder[];
-
     constructor(device: GPUDevice, submit: Submit)
     {
         super();
 
         this._onCreate(device, submit);
-        this._onMap(device, submit);
+        //
+        WGPUSubmit.map.set([device, submit], this);
+        this.destroyCall(() => { WGPUSubmit.map.delete([device, submit]); });
     }
 
     private _onCreate(device: GPUDevice, submit: Submit)
     {
         const r_submit = reactive(submit);
 
-        this.effect(() =>
+        const computedCommandBuffers = computed(() =>
         {
             r_submit.commandEncoders.concat();
 
-            this.commandBuffers = submit.commandEncoders.map((v) =>
+            const commandBuffers = submit.commandEncoders.map((v) =>
             {
                 const commandEncoderCommand = WGPUCommandEncoder.getInstance(device, v);
 
                 return commandEncoderCommand;
             });
+
+            return commandBuffers;
         });
+
+        this.run = (device: GPUDevice) =>
+        {
+            const commandBuffers = computedCommandBuffers.value;
+
+            reactive(device.queue).preSubmit = ~~device.queue.preSubmit + 1;
+
+            device.queue.submit(commandBuffers.map((v) => v.run(device)));
+
+            reactive(device.queue).afterSubmit = ~~device.queue.afterSubmit + 1;
+
+            // 派发提交WebGPU事件
+            anyEmitter.emit(device.queue, GPUQueue_submit);
+        }
     }
 
-    run(device: GPUDevice)
-    {
-        const { commandBuffers } = this;
-
-        reactive(device.queue).preSubmit = ~~device.queue.preSubmit + 1;
-
-        device.queue.submit(commandBuffers.map((v) => v.run(device)));
-
-        reactive(device.queue).afterSubmit = ~~device.queue.afterSubmit + 1;
-
-        // 派发提交WebGPU事件
-        anyEmitter.emit(device.queue, GPUQueue_submit);
-    }
-
-    private _onMap(device: GPUDevice, submit: Submit)
-    {
-        device.submitCommands ??= new WeakMap<Submit, WGPUSubmit>();
-        device.submitCommands.set(submit, this);
-        this.destroyCall(() => { device.submitCommands.delete(submit); });
-    }
+    run: (device: GPUDevice) => void;
 
     static getInstance(device: GPUDevice, submit: Submit)
     {
-        return device.submitCommands?.get(submit) || new WGPUSubmit(device, submit);
+        return WGPUSubmit.map.get([device, submit]) || new WGPUSubmit(device, submit);
     }
+    private static readonly map = new ChainMap<[GPUDevice, Submit], WGPUSubmit>();
 }
 
 declare global
 {
-    interface GPUDevice
-    {
-        submitCommands: WeakMap<Submit, WGPUSubmit>;
-    }
-
     interface GPUQueue
     {
         preSubmit: number;
