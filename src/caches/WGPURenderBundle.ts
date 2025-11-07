2d11c7fbe87052d1f818cb918fdf28a3a308c59a
feng <908087098@qq.com>
2025-11-07 19:58:54 +0800

    refactor: 重构 runRenderBundle 函数以简化渲染逻辑
    - 使用 WGPURenderBundle 的单例实例替代原有的 GPU 渲染捆绑获取逻辑
    - 移除冗余的 getGPURenderBundle 函数，提升代码清晰度
    - 简化渲染捆绑的执行过程，减少代码冗余

---
+++ src/caches/WGPURenderBundle.ts
@@ -0,0 +1,84 @@
+import { Computed, computed, reactive } from "@feng3d/reactivity";
+import { ChainMap } from "@feng3d/render-api";
+import { RenderBundle } from "../data/RenderBundle";
+import { RenderPassFormat } from "../internal/RenderPassFormat";
+import { runRenderObject } from "../internal/runRenderObject";
+import { WGPURenderObjectState, runCommands } from "../internal/WGPURenderObjectState";
+import { ReactiveObject } from "../ReactiveObject";
+
+export class WGPURenderBundle extends ReactiveObject
+{
+    get gpuRenderBundle() { return this._computedGpuRenderBundle.value; }
+
+    private _computedGpuRenderBundle: Computed<GPURenderBundle>;
+
+    constructor(device: GPUDevice, renderBundle: RenderBundle, renderPassFormat: RenderPassFormat, attachmentSize: { readonly width: number, readonly height: number })
+    {
+        super();
+        this._onCreate(device, renderBundle, renderPassFormat, attachmentSize);
+
+        //
+        WGPURenderBundle.map.set([device, renderBundle, renderPassFormat, attachmentSize], this);
+        this.destroyCall(() => { WGPURenderBundle.map.delete([device, renderBundle, renderPassFormat, attachmentSize]); });
+    }
+
+    private _onCreate(device: GPUDevice, renderBundle: RenderBundle, renderPassFormat: RenderPassFormat, attachmentSize: { readonly width: number, readonly height: number })
+    {
+        const r_renderBundle = reactive(renderBundle);
+        const r_renderPassFormat = reactive(renderPassFormat);
+
+        this._computedGpuRenderBundle = computed(() =>
+        {
+            // 执行
+            r_renderPassFormat.colorFormats.concat();
+            const descriptor: GPURenderBundleEncoderDescriptor = { colorFormats: renderPassFormat.colorFormats };
+            if (r_renderPassFormat.depthStencilFormat)
+            {
+                descriptor.depthStencilFormat = renderPassFormat.depthStencilFormat;
+            }
+            if (r_renderPassFormat.sampleCount)
+            {
+                descriptor.sampleCount = renderPassFormat.sampleCount;
+            }
+            if (r_renderBundle.descriptor?.depthReadOnly)
+            {
+                descriptor.depthReadOnly = true;
+            }
+            if (r_renderBundle.descriptor?.stencilReadOnly)
+            {
+                descriptor.stencilReadOnly = true;
+            }
+            //
+            const renderBundleEncoder = device.createRenderBundleEncoder(descriptor);
+
+            //
+            const bundleState = new WGPURenderObjectState(null, renderPassFormat, attachmentSize);
+
+            r_renderBundle.renderObjects.concat();
+            renderBundle.renderObjects.forEach((renderObject) =>
+            {
+                runRenderObject(device, renderPassFormat, attachmentSize, renderObject, bundleState);
+            });
+
+            const bundleCommands = bundleState.commands.filter((command) => (
+                command[0] !== 'setViewport'
+                && command[0] !== 'setScissorRect'
+                && command[0] !== 'setBlendConstant'
+                && command[0] !== 'setStencilReference'
+            ));
+
+            runCommands(renderBundleEncoder, bundleCommands);
+
+            const gpuRenderBundle = renderBundleEncoder.finish();
+
+            return gpuRenderBundle;
+        });
+    }
+
+    static getInstance(device: GPUDevice, renderBundle: RenderBundle, renderPassFormat: RenderPassFormat, attachmentSize: { readonly width: number, readonly height: number })
+    {
+        return this.map.get([device, renderBundle, renderPassFormat, attachmentSize]) || new WGPURenderBundle(device, renderBundle, renderPassFormat, attachmentSize);
+    }
+
+    private static readonly map = new ChainMap<[GPUDevice, RenderBundle, RenderPassFormat, { readonly width: number, readonly height: number }], WGPURenderBundle>();
+}
--- src/internal/WGPURenderBundle.ts
+++ src/internal/WGPURenderBundle.ts
@@ -1,78 +1,12 @@
-import { computed, Computed, reactive } from '@feng3d/reactivity';
-import { ChainMap } from '@feng3d/render-api';
+import { WGPURenderBundle } from '../caches/WGPURenderBundle';
 import { RenderBundle } from '../data/RenderBundle';
 import { RenderPassFormat } from './RenderPassFormat';
-import { runRenderObject } from './runRenderObject';
-import { CommandType, runCommands, WGPURenderObjectState } from './WGPURenderObjectState';
+import { CommandType, WGPURenderObjectState } from './WGPURenderObjectState';
 
 export function runRenderBundle(device: GPUDevice, commands: CommandType[], state: WGPURenderObjectState, renderBundle: RenderBundle, renderPassFormat: RenderPassFormat, attachmentSize: { readonly width: number, readonly height: number })
 {
-    const gpuRenderBundle = getGPURenderBundle(device, renderBundle, renderPassFormat, attachmentSize);
+    const wgpuRenderBundle = WGPURenderBundle.getInstance(device, renderBundle, renderPassFormat, attachmentSize);
+    const gpuRenderBundle = wgpuRenderBundle.gpuRenderBundle;
 
     commands.push(['executeBundles', [[gpuRenderBundle]]]);
 }
-
-export function getGPURenderBundle(device: GPUDevice, renderBundle: RenderBundle, renderPassFormat: RenderPassFormat, attachmentSize: { readonly width: number, readonly height: number })
-{
-    let computedGpuRenderBundle = map.get([device, renderBundle, renderPassFormat, attachmentSize]);
-    if (computedGpuRenderBundle)
-    {
-        return computedGpuRenderBundle.value;
-    }
-
-    const r_renderBundle = reactive(renderBundle);
-    const r_renderPassFormat = reactive(renderPassFormat);
-
-    computedGpuRenderBundle = computed(() =>
-    {
-        // 执行
-        r_renderPassFormat.colorFormats.concat();
-        const descriptor: GPURenderBundleEncoderDescriptor = { colorFormats: renderPassFormat.colorFormats };
-        if (r_renderPassFormat.depthStencilFormat)
-        {
-            descriptor.depthStencilFormat = renderPassFormat.depthStencilFormat;
-        }
-        if (r_renderPassFormat.sampleCount)
-        {
-            descriptor.sampleCount = renderPassFormat.sampleCount;
-        }
-        if (r_renderBundle.descriptor?.depthReadOnly)
-        {
-            descriptor.depthReadOnly = true;
-        }
-        if (r_renderBundle.descriptor?.stencilReadOnly)
-        {
-            descriptor.stencilReadOnly = true;
-        }
-        //
-        const renderBundleEncoder = device.createRenderBundleEncoder(descriptor);
-
-        //
-        const bundleState = new WGPURenderObjectState(null, renderPassFormat, attachmentSize);
-
-        r_renderBundle.renderObjects.concat();
-        renderBundle.renderObjects.forEach((renderObject) =>
-        {
-            runRenderObject(device, renderPassFormat, attachmentSize, renderObject, bundleState);
-        });
-
-        const bundleCommands = bundleState.commands.filter((command) => (
-            command[0] !== 'setViewport'
-            && command[0] !== 'setScissorRect'
-            && command[0] !== 'setBlendConstant'
-            && command[0] !== 'setStencilReference'
-        ));
-
-        runCommands(renderBundleEncoder, bundleCommands);
-
-        const gpuRenderBundle = renderBundleEncoder.finish();
-
-        return gpuRenderBundle;
-    });
-
-    map.set([device, renderBundle, renderPassFormat, attachmentSize], computedGpuRenderBundle);
-
-    return computedGpuRenderBundle.value;
-}
-
-const map = new ChainMap<[GPUDevice, RenderBundle, RenderPassFormat, { readonly width: number, readonly height: number }], Computed<GPURenderBundle>>();
