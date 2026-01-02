3e51c1d94cc43f69bc868903728d33c3ec89c03d
feng <908087098@qq.com>
2026-01-02 17:08:24 +0800

    chore: 更新依赖版本并优化配置
    - 更新 package.json 中 @feng3d/reactivity 依赖版本至 1.0.12，@feng3d/webgpu 版本至 0.0.13。
    - 删除 .gitmodules 文件，移除不再使用的子模块配置。
    - 在 .gitignore 中添加测试构建输出目录 test_dist。
    - 修改 tsconfig.json，设置 rootDir 为 src。
    - 更新 vitest.config.ts，添加测试环境设置。
    - 修改 postpublish.js 和 prepublish.js，使用 replaceAll 方法替换 package.json 中的路径。

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -6,13 +6,13 @@ const pkgpath = path.resolve('package.json');
 let pkg = fs.readFileSync(pkgpath, 'utf8');
 
 pkg = pkg
-    .replace(`"types": "./src/index.ts"`, `"types": "./lib/index.d.ts"`)
+    .replaceAll(`"types": "./src/index.ts"`, `"types": "./lib/index.d.ts"`)
     //
-    .replace(`"module": "./src/index.ts"`, `"module": "./dist/index.js"`)
-    .replace(`"main": "./src/index.ts"`, `"main": "./dist/index.umd.cjs"`)
+    .replaceAll(`"module": "./src/index.ts"`, `"module": "./dist/index.js"`)
+    .replaceAll(`"main": "./src/index.ts"`, `"main": "./dist/index.umd.cjs"`)
     //
-    .replace(`"import": "./src/index.ts"`, `"import": "./dist/index.js"`)
-    .replace(`"require": "./src/index.ts"`, `"require": "./dist/index.umd.cjs"`)
+    .replaceAll(`"import": "./src/index.ts"`, `"import": "./dist/index.js"`)
+    .replaceAll(`"require": "./src/index.ts"`, `"require": "./dist/index.umd.cjs"`)
 ;
 
 fs.writeFileSync(pkgpath, pkg, 'utf8');
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
-// 配置 Vitest 以适配项目
+// 配置Vitest以适配项目
 export default defineConfig({
     define: {
         __DEV__: true,
     },
+    // 设置测试环境
     test: {
         globals: true,
     },
